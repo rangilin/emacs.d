53b17f7dfe4d5b581748672baa69ceb1d232500b
Rangi Lin <rangiltw@gmail.com>
2013-05-26 23:45:09 +0800

    separate code by comments

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
 (add-to-list 'load-path user-emacs-directory)
 
+;; -------------------------------------------------- Initializing
 (require 'init-elpa)
 
+;; -------------------------------------------------- modes/features/packages
 (require 'init-ido)
 (require 'init-misc)
 
+;; -------------------------------------------------- config keybinding
 (require 'init-keybinding)
