d29817ca21b5f14b99d0be5fa497d77e044b2ca8
Rangi Lin <rangiltw@gmail.com>
2013-05-26 23:45:33 +0800

    backup file will now located at tmp

--- init-misc.el
+++ init-misc.el
@@ -1,11 +1,14 @@
 ;; hide toolbar
 (tool-bar-mode -1)
 
-
 ;; theme
 (load-theme 'tango t)
 
 ;; git
 (require-package 'magit)
+
+;; backup
+(setq backup-by-copying t)
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 
 (provide 'init-misc)
