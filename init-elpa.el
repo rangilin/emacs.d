f9e7e17a9491d28da7ec5ff26ba5513806ca7265
Rangi Lin <rangiltw@gmail.com>
2013-05-26 22:35:21 +0800

    initialize package

---
+++ init-elpa.el
@@ -0,0 +1,25 @@
+;;;; Initializing ELPA
+(require 'package)
+
+;; Repositories
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+;; On-demand installation of packages
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+
+(package-initialize)
+
+(provide 'init-elpa)
