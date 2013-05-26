40ffcb77f62771807f1b50b1fb365f90c9e1f9c1
Rangi Lin <rangiltw@gmail.com>
2013-05-26 21:33:24 +0800

    active ido and hide toolbar

---
+++ init.el
@@ -0,0 +1,9 @@
+;; hide toolbar
+(tool-bar-mode -1)
+
+;; ido 
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+(setq ido-use-virtual-buffers t)
+(setq ido-max-directory-size 100000)
