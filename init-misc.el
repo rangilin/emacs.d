c70f45e91e84b23b3f688e50a7dc42e9419229aa
Rangi Lin <rangiltw@gmail.com>
2013-05-28 00:01:35 +0800

    display trailing whitespacce, always indent with spaces

--- init-misc.el
+++ init-misc.el
@@ -11,4 +11,9 @@
 (setq backup-by-copying t)
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 
+;; editing
+(setq-default 
+ show-trailing-whitespace t
+ indent-tabs-mode nil)
+
 (provide 'init-misc)
