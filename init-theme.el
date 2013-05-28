737fed2d411cba48b5fd1808dc0d25055aa74f71
Rangi Lin <rangiltw@gmail.com>
2013-05-28 23:39:13 +0800

    remove solarize theme

--- init-theme.el
+++ init-theme.el
@@ -1,11 +1,13 @@
-;;;; Theme
-
-(require-package 'color-theme-sanityinc-solarized)
+;;----------------------------------------------------------------------------
+;; My favorite theme in Emacs so far
+;;----------------------------------------------------------------------------
 (require-package 'color-theme-sanityinc-tomorrow)
 
 (setq-default custom-enabled-themes '(sanityinc-tomorrow-eighties))
 
+;;----------------------------------------------------------------------------
 ;; Ensure that themes will be applied even if they have not been customized
+;;----------------------------------------------------------------------------
 (defun reapply-themes ()
   "Forcibly load the themes listed in `custom-enabled-themes'."
   (dolist (theme custom-enabled-themes)
@@ -14,5 +16,7 @@
   (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
 
 (add-hook 'after-init-hook 'reapply-themes)
+
+
 
 (provide 'init-theme)
