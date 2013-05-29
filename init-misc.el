f1c393a2153a1340874e13fca39108589906ae0d
Rangi Lin <rangiltw@gmail.com>
2013-05-29 23:07:48 +0800

    install rvm, yasnippet, exec-path-from-shell

--- init-misc.el
+++ init-misc.el
@@ -24,7 +24,21 @@
 ;; yes/no => y/n
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; rvm
+(require-package 'rvm)
+(require 'rvm)
 
+(rvm-use-default)
 
+;; yasnippet
+(require-package 'yasnippet)
+(require 'yasnippet)
+
+(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+(yas-global-mode 1) ; must after set directory so it can load snippets from there
+
+;; exec path
+(require-package 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
 
 (provide 'init-misc)
