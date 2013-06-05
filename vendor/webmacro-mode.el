149ff693f5831db9559a9ed7084835640751869f
Rangi Lin <rangiltw@gmail.com>
2013-06-06 01:00:31 +0800

    add webmacro keyword syntax

--- vendor/webmacro-mode.el
+++ vendor/webmacro-mode.el
@@ -3,7 +3,7 @@
 (define-generic-mode 'webmacro-mode
   '("##")
   '()
-  '()
+  '(("#if\\|#elseif\\|#else\\|#foreach\\|#begin\\|#end\\|#comment\\|#macro\\|#set\\|#include" . 'font-lock-keyword-face))
   '("\\.wm[m]?\'")
   nil
   "A generic mode for Webmacro"
