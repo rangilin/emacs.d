9758ac1b48f7b6b3fa7d0838b45a5a12cebaf5a3
Rangi Lin <rangiltw@gmail.com>
2013-06-12 13:59:57 +0800

    add prefix to the key name

--- init-key-definitions.el
+++ init-key-definitions.el
@@ -2,106 +2,106 @@
 
 ;; -------------------------------------------------- Cursor movements
 ;; move by chracter
-(defconst forward-char-key (kbd "M-l"))
-(defconst backward-char-key (kbd "M-j"))
+(defconst rl-forward-char-key (kbd "M-l"))
+(defconst rl-backward-char-key (kbd "M-j"))
 
 ;; move by word
-(defconst forward-word-key (kbd "M-o"))
-(defconst backward-word-key (kbd "M-u"))
+(defconst rl-forward-word-key (kbd "M-o"))
+(defconst rl-backward-word-key (kbd "M-u"))
 
 ;; move by line
-(defconst previous-line-key (kbd "M-i"))
-(defconst next-line-key (kbd "M-k"))
+(defconst rl-forward-line-key (kbd "M-k"))
+(defconst rl-backward-line-key (kbd "M-i"))
 
 ;; move by text block
-(defconst previous-block-key (kbd "M-U"))
-(defconst next-block-key (kbd "M-O"))
+(defconst rl-backward-block-key (kbd "M-U"))
+(defconst rl-forward-block-key (kbd "M-O"))
 
 ;; move by page
-(defconst scroll-cursor-up-key (kbd "M-I"))
-(defconst scroll-cursor-down-key (kbd "M-K"))
+(defconst rl-backward-page-key (kbd "M-I"))
+(defconst rl-forward-page-key (kbd "M-K"))
 
 ;; move to buffer top/bottom
-(defconst beginning-of-buffer-key (kbd "M-Y"))
-(defconst end-of-buffer-key (kbd "M-y"))
+(defconst rl-beginning-of-buffer-key (kbd "M-Y"))
+(defconst rl-end-of-buffer-key (kbd "M-y"))
 
 ;; move to line beginning/end
-(defconst back-to-indentation-or-beginning-of-line-key (kbd "M-H")
+(defconst rl-move-to-indentation-or-beginning-of-line-key (kbd "M-H")
   "Move to the indentation, move the the beginning of the line if repeated.")
-(defconst move-end-of-line-key (kbd "M-h"))
+(defconst rl-move-to-end-of-line-key (kbd "M-h"))
 
 ;; move to given line
-(defconst goto-line-key (kbd "C-c g"))
+(defconst rl-goto-line-key (kbd "C-c g"))
 
 ;; -------------------------------------------------- Editing
 ;; delete by character
-(defconst delete-backward-char-key (kbd "M-d"))
-(defconst delete-forward-char-key (kbd "M-f"))
+(defconst rl-delete-backward-char-key (kbd "M-d"))
+(defconst rl-delete-forward-char-key (kbd "M-f"))
 
 ;; delete by word
-(defconst backward-kill-word-key (kbd "M-e"))
-(defconst forward-kill-word-key (kbd "M-r"))
+(defconst rl-delete-backward-word-key (kbd "M-e"))
+(defconst rl-delete-forward-word-key (kbd "M-r"))
 
 ;; delete line
-(defconst kill-line-key (kbd "M-g"))
-(defconst kill-back-to-indentation-key (kbd "M-G"))
-(defconst kill-whole-line-key (kbd "C-S-g"))
+(defconst rl-delete-to-end-of-line-key (kbd "M-g"))
+(defconst rl-delete-to-indentation-of-line-key (kbd "M-G"))
+(defconst rl-delete-whole-line-key (kbd "C-S-g"))
 
 ;; insert newline above/below
-(defconst insert-newline-above-key (kbd "<M-S-return>"))
-(defconst insert-newline-below-key (kbd "<S-return>"))
+(defconst rl-insert-newline-above-key (kbd "<M-S-return>"))
+(defconst rl-insert-newline-below-key (kbd "<S-return>"))
 
 ;; copy, paste, cut
-(defconst kill-region-key (kbd "M-x"))
-(defconst kill-ring-save-key (kbd "M-c"))
-(defconst yank-key (kbd "M-v"))
-(defconst yank-pop-key (kbd "M-V"))
+(defconst rl-cut-key (kbd "M-x"))
+(defconst rl-copy-key (kbd "M-c"))
+(defconst rl-paste-key (kbd "M-v"))
+(defconst rl-paste-history-key (kbd "M-V"))
 
 ;; undo, redo
-(defconst undo-key (kbd "M-z"))
-(defconst redo-key (kbd "M-Z"))
+(defconst rl-undo-key (kbd "M-z"))
+(defconst rl-redo-key (kbd "M-Z"))
 
 ;; move text
-(defconst move-text-up-key (kbd "H-S-i"))
-(define-key input-decode-map (kbd "C-S-i") move-text-up-key)
-(defconst move-text-down-key (kbd "C-S-k"))
+(defconst rl-move-text-up-key (kbd "H-S-i"))
+(define-key input-decode-map (kbd "C-S-i") rl-move-text-up-key)
+(defconst rl-move-text-down-key (kbd "C-S-k"))
 
 ;; join lines
-(defconst join-line-below-key (kbd "C-j"))
-(defconst join-line-above-key (kbd "C-S-j"))
+(defconst rl-join-line-below-key (kbd "C-j"))
+(defconst rl-join-line-above-key (kbd "C-S-j"))
 
 ;; insert newline and indent
-(defconst newline-and-indent-key (kbd "RET"))
+(defconst rl-newline-and-indent-key (kbd "RET"))
 
 ;; toogle letter case of a word
-(defconst toggle-word-letter-case-key (kbd "C-M-c"))
+(defconst rl-toggle-word-letter-case-key (kbd "C-M-c"))
 
 ;; -------------------------------------------------- Others
 ;; save buffers
-(defconst save-buffer-key (kbd "C-s"))
-(defconst save-some-buffers-key (kbd "C-S-s"))
+(defconst rl-save-buffer-key (kbd "C-s"))
+(defconst rl-save-some-buffers-key (kbd "C-S-s"))
 
 ;; mark
-(defconst set-mark-command-key (kbd "M-."))
+(defconst rl-set-mark-command-key (kbd "M-."))
 
 ;; increment search
-(defconst isearch-forward-key (kbd "C-f"))
-(defconst isearch-backward-key (kbd "C-S-f"))
+(defconst rl-isearch-forward-key (kbd "C-f"))
+(defconst rl-isearch-backward-key (kbd "C-S-f"))
 
 ;; recenter
-(defconst recenter-key (kbd "M-p"))
+(defconst rl-recenter-key (kbd "M-p"))
 
 ;; execute extend command
-(defconst execute-extend-command-key (kbd "M-a"))
+(defconst rl-execute-extend-command-key (kbd "M-a"))
 
 ;; ibuffer
-(defconst ibuffer-key (kbd "C-x C-b"))
+(defconst rl-ibuffer-key (kbd "C-x C-b"))
 
 ;; window layout arrange
-(defconst split-window-vertically-key (kbd "C-x 2"))
-(defconst split-window-horizontally-key (kbd "C-x 3"))
-(defconst split-window-vertically-instead-key (kbd "C-x _"))
-(defconst split-window-horizontally-instead-key (kbd "C-x |"))
+(defconst rl-split-window-vertically-key (kbd "C-x 2"))
+(defconst rl-split-window-horizontally-key (kbd "C-x 3"))
+(defconst rl-split-window-vertically-instead-key (kbd "C-x _"))
+(defconst rl-split-window-horizontally-instead-key (kbd "C-x |"))
 
 
 (provide 'init-key-definitions)
--- init-keybinding.el
+++ init-keybinding.el
@@ -2,114 +2,120 @@
 ;; Movement
 ;;----------------------------------------------------------------------------
 ;; by line
-(global-set-key previous-line-key 'previous-line)
-(global-set-key next-line-key 'next-line)
+(global-set-key rl-forward-line-key 'next-line)
+(global-set-key rl-backward-line-key 'previous-line)
 
 ;; by character
-(global-set-key forward-char-key 'forward-char)
-(global-set-key backward-char-key 'backward-char)
+(global-set-key rl-forward-char-key 'forward-char)
+(global-set-key rl-backward-char-key 'backward-char)
 
 ;; by word
-(global-set-key forward-word-key 'forward-word)
-(global-set-key backward-word-key 'backward-word)
+(global-set-key rl-forward-word-key 'forward-word)
+(global-set-key rl-backward-word-key 'backward-word)
 
 ;; by block
-(global-set-key previous-block-key 'previous-block)
-(global-set-key next-block-key 'next-block)
+(global-set-key rl-forward-block-key 'next-block)
+(global-set-key rl-backward-block-key 'previous-block)
 
 ;; page up/down
-(global-set-key scroll-cursor-up-key 'scroll-cursor-up)
-(global-set-key scroll-cursor-down-key 'scroll-cursor-down)
+(global-set-key rl-forward-page-key 'scroll-cursor-down)
+(global-set-key rl-backward-page-key 'scroll-cursor-up)
 
 ;; to top/bottom of buffer
-(global-set-key beginning-of-buffer-key 'beginning-of-buffer)
-(global-set-key end-of-buffer-key 'end-of-buffer)
+(global-set-key rl-beginning-of-buffer-key 'beginning-of-buffer)
+(global-set-key rl-end-of-buffer-key 'end-of-buffer)
 
 ;; to beginning/end of line
-(global-set-key back-to-indentation-or-beginning-of-line-key
+(global-set-key rl-move-to-indentation-or-beginning-of-line-key
                 'back-to-indentation-or-beginning-of-line)
-(global-set-key move-end-of-line-key 'move-end-of-line)
+(global-set-key rl-move-to-end-of-line-key 'move-end-of-line)
 
 ;; goto line
-(global-set-key goto-line-key 'goto-line)
+(global-set-key rl-goto-line-key 'goto-line)
 
 ;;----------------------------------------------------------------------------
 ;; Deletion
 ;;----------------------------------------------------------------------------
 
 ;; by character
-(global-set-key delete-backward-char-key 'delete-backward-char)
-(global-set-key delete-forward-char-key 'delete-char)
+(global-set-key rl-delete-backward-char-key 'delete-backward-char)
+(global-set-key rl-delete-forward-char-key 'delete-char)
 
 ;; by word
-(global-set-key backward-kill-word-key 'backward-kill-word)
-(global-set-key forward-kill-word-key 'kill-word)
+(global-set-key rl-delete-backward-word-key 'backward-kill-word)
+(global-set-key rl-delete-forward-word-key 'kill-word)
 
 ;; kill line
-(global-set-key kill-line-key 'kill-line)
-(global-set-key kill-back-to-indentation-key 'kill-back-to-indentation)
-(global-set-key kill-whole-line-key 'kill-whole-line)
+(global-set-key rl-delete-to-end-of-line-key 'kill-line)
+(global-set-key rl-delete-to-indentation-of-line-key 'kill-back-to-indentation)
+(global-set-key rl-delete-whole-line-key 'kill-whole-line)
 
 ;;----------------------------------------------------------------------------
 ;; Editing
 ;;----------------------------------------------------------------------------
 
 ;; insert newline
-(global-set-key insert-newline-above-key 'insert-newline-above)
-(global-set-key insert-newline-below-key 'insert-newline-below)
+(global-set-key rl-insert-newline-above-key 'insert-newline-above)
+(global-set-key rl-insert-newline-below-key 'insert-newline-below)
 
 ;; copy, cut & paste
-(global-set-key kill-region-key 'kill-region)
-(global-set-key kill-ring-save-key 'kill-ring-save)
-(global-set-key yank-key 'yank)
-(global-set-key yank-pop-key 'yank-pop)
+(global-set-key rl-cut-key 'kill-region)
+(global-set-key rl-copy-key 'kill-ring-save)
+(global-set-key rl-paste-key 'yank)
+(global-set-key rl-paste-history-key 'yank-pop)
 
 ;; undo/redo
-(global-set-key undo-key 'undo-tree-undo)
-(global-set-key redo-key 'undo-tree-redo)
+(global-set-key rl-undo-key 'undo-tree-undo)
+(global-set-key rl-redo-key 'undo-tree-redo)
 
 ;; move-text
-(global-set-key move-text-up-key 'move-text-up)
-(global-set-key move-text-down-key 'move-text-down)
+(global-set-key rl-move-text-up-key 'move-text-up)
+(global-set-key rl-move-text-down-key 'move-text-down)
 
-;; save
-(global-set-key save-buffer-key 'save-buffer)
-(global-set-key save-some-buffers-key 'save-some-buffers)
+;; join line
+(global-set-key rl-join-line-below-key (lambda () (interactive) (join-line 1)))
+(global-set-key rl-join-line-above-key 'join-line)
+
+;; newline & indent
+(global-set-key rl-newline-and-indent-key 'newline-and-indent)
+
+;; toggle word case
+(global-set-key rl-toggle-word-letter-case-key `toggle-letter-case)
+
 
 ;;----------------------------------------------------------------------------
 ;; Others
 ;;----------------------------------------------------------------------------
+;; save
+(global-set-key rl-save-buffer-key 'save-buffer)
+(global-set-key rl-save-some-buffers-key 'save-some-buffers)
 
 ;; Mark
-(global-set-key set-mark-command-key 'set-mark-command)
+(global-set-key rl-set-mark-command-key 'set-mark-command)
 
 ;; search
-(global-set-key isearch-forward-key 'isearch-forward)
-(global-set-key isearch-backward-key 'isearch-backward)
-(define-key isearch-mode-map isearch-forward-key 'isearch-repeat-forward)
-(define-key isearch-mode-map isearch-backward-key 'isearch-repeat-backward)
+(global-set-key rl-isearch-forward-key 'isearch-forward)
+(global-set-key rl-isearch-backward-key 'isearch-backward)
+(define-key isearch-mode-map rl-isearch-forward-key 'isearch-repeat-forward)
+(define-key isearch-mode-map rl-isearch-backward-key 'isearch-repeat-backward)
 
 ;; recenter
-(global-set-key recenter-key 'recenter-top-bottom)
+(global-set-key rl-recenter-key 'recenter-top-bottom)
 
 ;;----------------------------------------------------------------------------
-(global-set-key join-line-below-key (lambda () (interactive) (join-line 1)))
-(global-set-key join-line-above-key 'join-line)
 
-(global-set-key execute-extend-command-key 'smex)
+(global-set-key rl-execute-extend-command-key 'smex)
 
-(global-set-key ibuffer-key 'ibuffer)
+(global-set-key rl-ibuffer-key 'ibuffer)
 
-(global-set-key newline-and-indent-key 'newline-and-indent)
 
-(global-set-key split-window-vertically-key
+(global-set-key rl-split-window-vertically-key
                 (focus-and-show-other-buffer-after 'split-window-vertically))
-(global-set-key split-window-horizontally-key
+(global-set-key rl-split-window-horizontally-key
                 (focus-and-show-other-buffer-after 'split-window-horizontally))
-(global-set-key split-window-horizontally-instead-key 'split-window-horizontally-instead)
-(global-set-key split-window-vertically-instead-key 'split-window-vertically-instead)
+(global-set-key rl-split-window-horizontally-instead-key 'split-window-horizontally-instead)
+(global-set-key rl-split-window-vertically-instead-key 'split-window-vertically-instead)
 
-(global-set-key toggle-word-letter-case-key `toggle-letter-case)
 
 
 (provide 'init-keybinding)
