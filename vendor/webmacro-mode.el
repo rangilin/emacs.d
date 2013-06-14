b8c04231b8c30a63dd14506e396d994c453b4e4b
Rangi Lin <rangiltw@gmail.com>
2013-06-15 01:01:00 +0800

    add function highlight make variable only highlight valid variable words

--- vendor/webmacro-mode.el
+++ vendor/webmacro-mode.el
@@ -4,10 +4,11 @@
   '("##")
   '()
   '(("#if\\|#elseif\\|#else\\|#foreach\\|#begin\\|#end\\|#comment\\|#macro\\|#set\\|#include\\|#text\\|#alternate" . 'font-lock-keyword-face)
-    ("\\$\\w+" . 'font-lock-variable-name-face))
+    ("\\$[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-variable-name-face)
+    ("#[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-function-name-face))
   '("\\.wm[m]?\'")
   nil
-  "A generic mode for Webmacro"
+  "A minimum mode for display Webmacro syntax highlight"
   )
 
 (provide 'webmacro-mode)
