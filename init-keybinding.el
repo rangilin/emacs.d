96d0a3ba07bcaf09b3b97de08c2491d223cf06ea
Rangi Lin <rangiltw@gmail.com>
2013-06-17 16:14:18 +0800

    add scroll other window

--- init-keybinding.el
+++ init-keybinding.el
@@ -23,6 +23,9 @@
 (global-set-key (kbd "M-<") 'scroll-cursor-up)
 (global-set-key (kbd "M->") 'scroll-cursor-down)
 
+(global-set-key (kbd "C-M-<") (lambda() (interactive) (scroll-other-window `-)))
+(global-set-key (kbd "C-M->") 'scroll-other-window)
+
 ;; to top/bottom of buffer
 (global-set-key (kbd "M-Y") 'beginning-of-buffer)
 (global-set-key (kbd "M-y") 'end-of-buffer)
