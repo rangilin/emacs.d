21e48fe7e4f8fc3665d00f44afe8bcc0ebff029e
Rangi Lin <rangiltw@gmail.com>
2013-07-13 23:16:29 +0800

    make tab key insert tab directly in webmacro mode

--- init-webmacro-mode.el
+++ init-webmacro-mode.el
@@ -3,6 +3,9 @@
 ;; active on files with .wm .wmm extension
 (add-to-list 'auto-mode-alist '("\\.wm[m]?\\'" . webmacro-mode))
 
+(eval-after-load "webmacro-mode"
+  '(setq indent-line-function 'insert-tab))
+
 (provide 'init-webmacro-mode)
 
 
