6f41e291a036f971cd0c0edb0d2f4cadb59da7ab
Rangi Lin <rangiltw@gmail.com>
2013-07-13 23:16:04 +0800

    add keymap and hook in webmacro mode

--- vendor/webmacro-mode.el
+++ vendor/webmacro-mode.el
@@ -1,5 +1,16 @@
 (require 'generic-x)
 
+(defvar webmacro-mode-hook nil)
+
+(defvar webmacro-mode-map
+  (let ((map (make-sparse-keymap)))
+    map)
+  "Keymap for webmacro-mode")
+
+(defun _init-webmacro ()
+  (use-local-map webmacro-mode-map)
+  (run-hooks webmacro-mode-hook))
+
 (define-generic-mode 'webmacro-mode
   '("##")
   '()
@@ -7,7 +18,7 @@
     ("\\$[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-variable-name-face)
     ("#[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-function-name-face))
   '("\\.wm[m]?\'")
-  nil
+  (list `_init-webmacro)
   "A minimum mode for display Webmacro syntax highlight"
   )
 
