d17999b977a7b65e7f094c7a086aab09daf93c7f
Rangi Lin <rangiltw@gmail.com>
2013-07-20 23:11:57 +0800

    rewrite webmacro mode by deriving from fundamental mode

--- vendor/webmacro-mode.el
+++ vendor/webmacro-mode.el
@@ -1,5 +1,3 @@
-(require 'generic-x)
-
 (defvar webmacro-mode-hook nil)
 
 (defvar webmacro-mode-map
@@ -7,19 +5,24 @@
     map)
   "Keymap for webmacro-mode")
 
+(defvar webmacro-keywords
+  (list
+   '("##.+\\|#comment \{\\(.*\n\\)+}\\|#comment #begin\\(.*\n\\)+#end" . 'font-lock-comment-face)
+   '("#if\\|#elseif\\|#else\\|#foreach\\|#begin\\|#end\\|#comment\\|#macro\\|#set\\|#include\\|#text\\|#alternate" . 'font-lock-keyword-face)
+   '("\\$[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-variable-name-face)
+   '("#[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-function-name-face)))
+
 (defun _init-webmacro ()
   (use-local-map webmacro-mode-map)
-  (run-hooks 'webmacro-mode-hook))
+  (run-hooks 'webmacro-mode-hook)
+  (set (make-local-variable 'font-lock-defaults) '(webmacro-keywords))
+  (set (make-local-variable 'font-lock-multiline) t)
+  (setq comment-start "##"))
 
-(define-generic-mode 'webmacro-mode
-  '("##")
-  '()
-  '(("#if\\|#elseif\\|#else\\|#foreach\\|#begin\\|#end\\|#comment\\|#macro\\|#set\\|#include\\|#text\\|#alternate" . 'font-lock-keyword-face)
-    ("\\$[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-variable-name-face)
-    ("#[a-zA-Z]+[A-Za-z0-9_]*" . 'font-lock-function-name-face))
-  '("\\.wm[m]?\'")
-  (list `_init-webmacro)
-  "A minimum mode for display Webmacro syntax highlight"
-  )
+
+(define-derived-mode webmacro-mode fundamental-mode "WM"
+  "Webmacro mode is a major mode for editing webmacro template files"
+  (_init-webmacro))
+
 
 (provide 'webmacro-mode)
