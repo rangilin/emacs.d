15b53c5dd354f1a9d4c53c443d807249ef24f7e8
Rangi Lin <rangiltw@gmail.com>
2013-07-26 09:35:44 +0800

    remove insert tab function for now

--- init-webmacro-mode.el
+++ init-webmacro-mode.el
@@ -3,11 +3,5 @@
 ;; active on files with .wm .wmm extension
 (add-to-list 'auto-mode-alist '("\\.wm[m]?\\'" . webmacro-mode))
 
-(add-hook 'webmacro-mode-hook
-          (lambda ()
-            (progn
-              (setq indent-line-function 'insert-tab))))
-
-
 (provide 'init-webmacro-mode)
 
