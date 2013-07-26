3cc1bb023cbb26826def67e2e91844856791e253
Rangi Lin <rangiltw@gmail.com>
2013-07-27 00:54:24 +0800

    change webmacro file location

--- init-webmacro-mode.el
+++ init-webmacro-mode.el
@@ -1,13 +1,9 @@
+(add-to-list 'load-path (expand-file-name "vendor/webmacro-mode" user-emacs-directory))
+
 (require 'webmacro-mode)
 
 ;; active on files with .wm .wmm extension
 (add-to-list 'auto-mode-alist '("\\.wm[m]?\\'" . webmacro-mode))
-
-(add-hook 'webmacro-mode-hook
-          (lambda ()
-            (progn
-              (setq indent-line-function 'insert-tab))))
-
 
 (provide 'init-webmacro-mode)
 
