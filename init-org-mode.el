bc9dbac6918b50efeb3d236e47010f84782fc2a0
Rangi Lin <rangiltw@gmail.com>
2013-07-27 23:50:03 +0800

    make org show invisible edit

--- init-org-mode.el
+++ init-org-mode.el
@@ -1,4 +1,7 @@
 (add-to-list 'load-path (expand-file-name "vendor/org/lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "vendor/org/contrib/lisp" user-emacs-directory))
 
+(setq-default org-catch-invisible-edits t)
+
+
 (provide `init-org-mode)
