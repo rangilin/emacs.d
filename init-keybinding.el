c15bec3a791f4dcc32dc389e6b3d0674042d84eb
Rangi Lin <rangiltw@gmail.com>
2013-09-03 23:30:11 +0800

    change multiple-cursor keys

--- init-keybinding.el
+++ init-keybinding.el
@@ -26,7 +26,8 @@
 ;; multiple-cursor
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-c C->") 'mc/mark-more-like-this-extended)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
 ;; -------------------------------------------------- Text manipulation
 ;; kill line back
