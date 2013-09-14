4e6649156139e32c3c909b8708145a19a88ea1ed
Rangi Lin <rangiltw@gmail.com>
2013-09-14 23:36:39 +0800

    remove apply theme on startup

--- init-theme.el
+++ init-theme.el
@@ -6,21 +6,5 @@
 (require-package 'gandalf-theme)
 (require-package 'zenburn-theme)
 
-;(setq-default custom-enabled-themes '(zenburn))
-
-
-;----------------------------------------------------------------------------
-; Ensure that themes will be applied even if they have not been customized
-;----------------------------------------------------------------------------
-;(defun reapply-themes ()
-;  "Forcibly load the themes listed in `custom-enabled-themes'."
-;  (dolist (theme custom-enabled-themes)
-;    (unless (custom-theme-p theme)
-;      (load-theme theme)))
-;  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
-;
-;(add-hook 'after-init-hook 'reapply-themes)
-
-
 
 (provide 'init-theme)
