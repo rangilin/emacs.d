e56849a33ebcc01e7fc97ef99ff7541f4bac8114
Rangi Lin <rangiltw@gmail.com>
2013-09-18 23:29:38 +0800

    enable visual-line, turn-off hi-line

--- init-editing.el
+++ init-editing.el
@@ -8,8 +8,8 @@
  visible-bell 1
  comint-scroll-to-bottom-on-output 'all)
 
-(global-hl-line-mode 1)
-
+;(global-hl-line-mode 1)
+(global-visual-line-mode 1)
 ;; turn off whitespace visualization in some modes
 ;; --------------------------------------------------
 (dolist (hook '(eshell-mode-hook
