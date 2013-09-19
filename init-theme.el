b2f23d9ae570bd9324f842964860973e36f60ff8
Rangi Lin <rangiltw@gmail.com>
2013-09-19 10:46:02 +0800

    add soft-morning theme

--- init-theme.el
+++ init-theme.el
@@ -4,6 +4,7 @@
 (require-package 'color-theme-sanityinc-tomorrow)
 (require-package 'monokai-theme)
 (require-package 'gandalf-theme)
+(require-package 'soft-morning-theme)
 (require-package 'zenburn-theme)
 
 
