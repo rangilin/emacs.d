f4c1bd0d11f65956198af6a380b43777ce1dc694
Rangi Lin <rangiltw@gmail.com>
2013-10-07 23:48:26 +0800

    enable web-mode in html

--- init-web.el
+++ init-web.el
@@ -1,6 +1,9 @@
+(require 'init-elpa)
+
 (require-package 'simple-httpd)
 (require-package 'impatient-mode)
-(require-package 'web-mode)
 
+(require-package 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 (provide 'init-web)
