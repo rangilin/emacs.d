2e32c8edfd83fc6cfda36c500bec551d6a9cc1df
Rangi Lin <rangiltw@gmail.com>
2013-10-12 00:28:24 +0800

    remove impatient mode

--- init-web.el
+++ init-web.el
@@ -1,8 +1,5 @@
 (require 'init-elpa)
 
-(require-package 'simple-httpd)
-(require-package 'impatient-mode)
-
 (require-package 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
