a094f864cd075706678f7662a3b7a5308b3ab0d2
Rangi Lin <rangiltw@gmail.com>
2013-12-12 09:13:04 +0800

    merge duplicate ruby-mode setup

--- init.el
+++ init.el
@@ -205,43 +205,32 @@
     (setq-default projectile-switch-project-action 'projectile-dired)))
 
 (use-package ruby-mode
+  :mode (("\\.rake$" . ruby-mode)
+         ("\\.ru$" . ruby-mode)
+         ("Rakefile$" . ruby-mode))
   :init
   (progn
-    (bind-key "RET" 'reindent-then-newline-and-indent ruby-mode-map)
-    (bind-key "TAB" 'indent-for-tab-command ruby-mode-map)
     (use-package rvm
+      ;; :init (rvm-use-default)
       :config
       (progn
         (add-hook 'ruby-mode-hook 'rvm-activate-corresponding-ruby)))
+    (use-package rhtml-mode
+      :mode (("\\.html\\.erb$" . rhtml-mode)))
+    (use-package yari)
     (use-package rspec-mode
       :config
       (progn
         (defvar rspec-use-rvm t)))
-  :config
-  (progn
-    (custom-set-variables
-     '(ruby-insert-encoding-magic-comment nil)))))
-
-(use-package ruby-mode
-  :init
-  (progn
-    (use-package rvm
-      :init (rvm-use-default))
-    (use-package yari)
-    (use-package rhtml-mode
-      :mode (("\\.html\\.erb$" . rhtml-mode)))
     (use-package rinari
       :init (global-rinari-mode 1)
-      :config (setq ruby-insert-encoding-magic-comment nil))
-    (use-package rspec-mode
-      :config
-      (progn
-        (setq-default rspec-use-rvm t)
-        (setq-default rspec-use-rake-flag nil))))
+      :config (setq ruby-insert-encoding-magic-comment nil)))
   :config
   (progn
+    (bind-key "RET" 'reindent-then-newline-and-indent ruby-mode-map)
+    (bind-key "TAB" 'indent-for-tab-command ruby-mode-map)
     (setq ruby-deep-indent-paren nil)
-    (add-hook 'ruby-mode-hook 'rvm-activate-corresponding-ruby)))
+    (custom-set-variables '(ruby-insert-encoding-magic-comment nil))))
 
 (use-package sql
   :init
