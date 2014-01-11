c261a4c52e526ae7cd1bdbe5913992862c0431ea
Rangi Lin <rangiltw@gmail.com>
2014-01-11 17:07:53 +0800

    disable flyspell

--- init.el
+++ init.el
@@ -99,20 +99,20 @@
 (use-package flycheck-cask
   :init (add-hook 'flycheck-mode-hook 'flycheck-cask-setup))
 
-(use-package flyspell
-  :diminish ""
-  :bind
-  (("C-M-$" . flyspell-buffer)
-   ("C-$" . flyspell-check-previous-highlighted-word))
-  :init
-  (progn
-    (setq ispell-dictionary "english")
-    (add-hook 'markdown-mode-hook 'flyspell-mode)
-    (add-hook 'monky-log-edit-mode-hook 'flyspell-mode)
-    (add-hook 'prog-mode-hook 'flyspell-prog-mode))
-  :config
-  (progn
-    (bind-key "C-;" nil flyspell-mode-map))) ; reserved for ace jump
+;; (use-package flyspell
+;;   :diminish ""
+;;   :bind
+;;   (("C-M-$" . flyspell-buffer)
+;;    ("C-$" . flyspell-check-previous-highlighted-word))
+;;   :init
+;;   (progn
+;;     (setq ispell-dictionary "english")
+;;     (add-hook 'markdown-mode-hook 'flyspell-mode)
+;;     (add-hook 'monky-log-edit-mode-hook 'flyspell-mode)
+;;     (add-hook 'prog-mode-hook 'flyspell-prog-mode))
+;;   :config
+;;   (progn
+;;     (bind-key "C-;" nil flyspell-mode-map))) ; reserved for ace jump
 
 (use-package fullscreen-mode
   :init (fullscreen-mode 1))
