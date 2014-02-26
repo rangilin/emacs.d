95419c43797834f10f644fc6c25ab7705eb13413
Rangi Lin <rangiltw@gmail.com>
2014-02-26 23:10:51 +0800

    setup js mode indentation offset

--- setups/setup-js.el
+++ setups/setup-js.el
@@ -1,13 +1,16 @@
 (require 'use-package)
 
 (use-package js
-  :mode (("\\.json$" . js-mode)))
+  :mode (("\\.json$" . js-mode))
+  :config
+  (progn
+    (setq-default js-indent-level tab-width)))
 
 (use-package js2-mode
   :mode (("\\.js$" . js2-mode))
   :config
   (progn
-    (setq-default js2-basic-offset 2)
+    (setq-default js2-basic-offset tab-width)
     (setq-default js2-bounce-indent-p t)))
 
 (provide 'setup-js)
