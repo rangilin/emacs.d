16b423a9a21c129bd9f5f47e05a463229a72a964
Rangi Lin <rangiltw@gmail.com>
2014-03-12 21:13:11 +0800

    refactoring, open recent file in other window

--- lisp/setup-recentf.el
+++ lisp/setup-recentf.el
@@ -7,12 +7,23 @@
     (setq-default recentf-save-file (expand-file-name "recentf" rl/gen-dir))
     (setq-default recentf-max-saved-items 100)
     (recentf-mode 1))
-  :bind ("C-x C-r" . rl/recentf-ido-find-file))
+  :bind (("C-x C-r" . rl/recentf-ido-find-file)
+         ("C-x 4 r" . rl/recentf-ido-find-file-other-window)))
 
 (defun rl/recentf-ido-find-file ()
-  "Find a recent file using Ido."
+  "Find a recent file using ido."
   (interactive)
+  (rl--recentf-ido 'find-file))
+
+(defun rl/recentf-ido-find-file-other-window ()
+  "Find a recent file using ido and open it at other window"
+  (interactive)
+  (rl--recentf-ido 'find-file-other-window))
+
+(defun rl--recentf-ido (find-file-function)
+  "Using ido to find recent file with specified function"
   (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
     (when file
-      (find-file file))))
+      (funcall find-file-function file))))
+
 (provide 'setup-recentf)
