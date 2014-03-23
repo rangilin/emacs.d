ec230139ac0130159d1812176eb7ef83bc0b51db
Rangi Lin <rangiltw@gmail.com>
2014-03-24 00:10:01 +0800

    make double quote auto close in nxml mode

--- lisp/setup-nxml.el
+++ lisp/setup-nxml.el
@@ -8,12 +8,17 @@
   (progn
     (setq-default nxml-child-indent tab-width)
     (setq-default nxml-outline-child-indent tab-width)
-    (setq-default nxml-slash-auto-complete-flag t)))
+    (setq-default nxml-slash-auto-complete-flag t)
+    (defun rangi--modify-nxml-syntax ()
+      (modify-syntax-entry ?\" "\""))
+    (add-hook 'nxml-mode-hook 'rangi--modify-nxml-syntax)))
+
 
 (use-package sgml-mode
   :config
   (progn
     (bind-key "C-c r" 'mc/mark-sgml-tag-pair html-mode-map)))
+
 
 (use-package zencoding-mode
   :init
