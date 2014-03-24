e5fe21ad199e29e90da6dca1d6f4dfd5d8e3ad57
Rangi Lin <rangiltw@gmail.com>
2014-03-24 23:06:49 +0800

    enable and setup flyspell

--- lisp/setup-flyspell.el
+++ lisp/setup-flyspell.el
@@ -1,18 +1,20 @@
 (require 'use-package)
 
-;; (use-package flyspell
-;;   :diminish ""
-;;   :bind
-;;   (("C-M-$" . flyspell-buffer)
-;;    ("C-$" . flyspell-check-previous-highlighted-word))
-;;   :init
-;;   (progn
-;;     (setq ispell-dictionary "english")
-;;     (add-hook 'markdown-mode-hook 'flyspell-mode)
-;;     (add-hook 'monky-log-edit-mode-hook 'flyspell-mode)
-;;     (add-hook 'prog-mode-hook 'flyspell-prog-mode))
-;;   :config
-;;   (progn
-;;     (bind-key "C-;" nil flyspell-mode-map))) ; reserved for ace jump
+(use-package flyspell
+  :diminish ""
+  :bind
+  (("C-M-$" . flyspell-buffer)
+   ("C-$" . flyspell-check-previous-highlighted-word))
+  :init
+  (progn
+    (setq-default ispell-dictionary "english")
+    (setq-default flyspell-issue-message-flag nil)
+    (add-hook 'markdown-mode-hook 'flyspell-mode)
+    (add-hook 'monky-log-edit-mode-hook 'flyspell-mode)
+    (add-hook 'org-mode-hook 'flyspell-mode)
+    (add-hook 'prog-mode-hook 'flyspell-prog-mode))
+  :config
+  (progn
+    (bind-key "C-;" nil flyspell-mode-map))) ; reserved for ace jump
 
 (provide 'setup-flyspell)
