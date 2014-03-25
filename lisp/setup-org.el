9ad9152655b45a366c85d1e76828b15318d4584d
Rangi Lin <rangiltw@gmail.com>
2014-03-25 14:57:49 +0800

    move org settings to :init

--- lisp/setup-org.el
+++ lisp/setup-org.el
@@ -4,21 +4,23 @@
   :mode ("\\.org$" . org-mode)
   :init
   (progn
-    (use-package gnuplot)
-    (use-package org-plot
-      :bind (("C-M-g" . org-plot/gnuplot))))
-  :config
-  (progn
     (setq-default org-directory "/ramsey/Dropbox/org")
     (setq-default org-special-ctrl-a/e t)
     (setq-default org-startup-truncated nil)
     (setq-default org-startup-with-inline-images t)
     (setq-default org-drawers `("PROPERTIES" "CLOCK" "LOGBOOK" "RESULTS" "DETAIL"))
 
-    ;; workaround windmove conflict
     (add-hook 'org-shiftup-final-hook 'windmove-up)
     (add-hook 'org-shiftleft-final-hook 'windmove-left)
     (add-hook 'org-shiftdown-final-hook 'windmove-down)
-    (add-hook 'org-shiftright-final-hook 'windmove-right)))
+    (add-hook 'org-shiftright-final-hook 'windmove-right))
+
+
+    (use-package gnuplot)
+
+
+    (use-package org-plot
+      :bind (("C-M-g" . org-plot/gnuplot))))
+
 
 (provide 'setup-org)
