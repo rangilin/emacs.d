febdd07495f1ea9cade81138b24669c3b4aab9d7
Rangi Lin <rangiltw@gmail.com>
2014-04-07 00:04:41 +0800

    autopair will now always insert pair in org-mode

--- lisp/setup-autopair.el
+++ lisp/setup-autopair.el
@@ -3,6 +3,12 @@
 (use-package autopair
   :init
   (progn
-    (autopair-global-mode)))
+    (autopair-global-mode)
+
+    ;; make autopair always insert pair, so *) syntax in org-mode won't screw up autopair
+    (add-hook 'org-mode-hook
+              #'(lambda ()
+                  (set (make-local-variable 'autopair-pair-criteria) 'always)
+                  (set (make-local-variable 'autopair-skip-criteria) 'always)))))
 
 (provide 'setup-autopair)
