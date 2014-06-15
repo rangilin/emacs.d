057da27921315c11273fbe453665602079a37ef5
Rangi Lin <rangiltw@gmail.com>
2014-06-16 00:11:30 +0800

    disable autopair in term

--- lisp/setup-autopair.el
+++ lisp/setup-autopair.el
@@ -5,10 +5,14 @@
   (progn
     (autopair-global-mode)
 
+    (defun rangi/autopair-always-pair ()
+      (set (make-local-variable 'autopair-pair-criteria) 'always)
+      (set (make-local-variable 'autopair-skip-criteria) 'always))
     ;; make autopair always insert pair, so *) syntax in org-mode won't screw up autopair
-    (add-hook 'org-mode-hook
-              #'(lambda ()
-                  (set (make-local-variable 'autopair-pair-criteria) 'always)
-                  (set (make-local-variable 'autopair-skip-criteria) 'always)))))
+    (add-hook 'org-mode-hook 'rangi/autopair-always-pair)
+
+    (defun rangi/autopair-turn-off ()
+      (autopair-mode -1))
+    (add-hook 'term-mode-hook 'rangi/autopair-turn-off)))
 
 (provide 'setup-autopair)
