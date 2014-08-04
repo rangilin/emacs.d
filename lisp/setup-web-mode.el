4ad1776b5f649e8ebbffb7d5941a18daa44142a0
Rangi Lin <rangilin@otiga.com>
2014-08-04 18:29:03 +0800

    use tab in web mode

--- lisp/setup-web-mode.el
+++ lisp/setup-web-mode.el
@@ -7,12 +7,17 @@
          ("\\.mustache\\'" . web-mode)
          ("\\.djhtml\\'" . web-mode)
          ("\\.html\\'" . web-mode))
+  :init
+  (progn
+    (defun rangi/web-mode-hook ()
+      (setq indent-tabs-mode t))
+    (add-hook 'web-mode-hook 'rangi/web-mode-hook))
+
   :config
   (progn
     (autopair-mode -1) ;; use web-mode's auto pairing
     (setq-default web-mode-markup-indent-offset tab-width)
     (setq-default web-mode-css-indent-offset tab-width)
-    (setq-default web-mode-code-indent-offset tab-width)
-    ))
+    (setq-default web-mode-code-indent-offset tab-width)))
 
 (provide 'setup-web-mode)
