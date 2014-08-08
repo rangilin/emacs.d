a044399c5c7e3dc753817b20d8b986f5a21cc8b7
Rangi Lin <rangilin@otiga.com>
2014-08-08 16:22:07 +0800

    remove hook

--- lisp/setup-php.el
+++ lisp/setup-php.el
@@ -3,10 +3,6 @@
 (use-package php-mode
   :init
   (progn
-	(defun rangi/php-mode-hook ()
-	  (setq indent-tabs-mode t))
-	(add-hook 'php-mode-hook 'rangi/php-mode-hook)
-
     (bind-key "C-M-h" 'rangi/backward-delete-word php-mode-map)))
 
 (provide 'setup-php)
