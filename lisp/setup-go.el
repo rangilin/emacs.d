4ed8fe78457c17813bbb471d50f23669d98d9ad4
Rangi Lin <rangiltw@gmail.com>
2014-08-26 21:26:48 +0800

    always make go mode use 4 space width tab

--- lisp/setup-go.el
+++ lisp/setup-go.el
@@ -4,6 +4,12 @@
   :mode (("\\.go$" . go-mode))
   :init
   (progn
+
+	(defun rangi/go-mode-hook ()
+	  (setq tab-width 4))
+	(add-hook 'go-mode-hook 'rangi/go-mode-hook)
+
+	;; go-eldoc
     (use-package go-eldoc
       :config
       (progn
@@ -11,6 +17,7 @@
   :config
   (progn
     (add-hook 'before-save-hook 'gofmt-before-save)
+    (setq tab-width 4)
     (setq indent-tabs-mode t)))
 
 (provide 'setup-go)
