2b81150f1c1aab9dc4d560b07c9a984da375c2bd
Rangi Lin <rangiltw@gmail.com>
2014-08-30 01:23:08 +0800

    add move 5 lines/chars key

--- lisp/setup-cursor.el
+++ lisp/setup-cursor.el
@@ -67,4 +67,10 @@
 (bind-key "M-a" 'rangi/backward-paragraph)
 (bind-key "M-e" 'rangi/forward-paragraph)
 
+;; ------------------------------ move around a little faster
+(bind-key "C-S-n" (lambda () (interactive) (ignore-errors (forward-line 5))))
+(bind-key "C-S-p" (lambda () (interactive) (ignore-errors (forward-line -5))))
+(bind-key "C-S-f" (lambda () (interactive) (ignore-errors (forward-char 5))))
+(bind-key "C-S-b" (lambda () (interactive) (ignore-errors (backward-char 5))))
+
 (provide 'setup-cursor)
