604209e42cbea93bffbdb84677aa1e4786cd7c4b
Rangi Lin <rangiltw@gmail.com>
2014-08-31 19:52:01 +0800

    change how byte compile is done to avoid snippets get compiled

--- lisp/setup-elisp.el
+++ lisp/setup-elisp.el
@@ -32,7 +32,17 @@
 (defun rangi/byte-recompile ()
   "Byte-compile init files to improve speed"
   (interactive)
-  (byte-recompile-directory "~/.emacs.d" 0))
+  (byte-recompile-file (expand-file-name "init.el" user-emacs-directory))
+  (byte-recompile-file (expand-file-name "functions.el" user-emacs-directory))
+  (byte-recompile-file (expand-file-name "variables.el" user-emacs-directory))
+  (byte-recompile-file (expand-file-name "local-functions.el" user-emacs-directory))
+  (byte-recompile-file (expand-file-name "local-variables.el" user-emacs-directory))
+  (byte-recompile-directory (expand-file-name "lisp" user-emacs-directory) 0)
+  (byte-recompile-directory (expand-file-name "site-lisp" user-emacs-directory) 0))
+
+;; Command to find all elc file :
+;; find . -name "*.elc" -type f -not -path "./.cask/*"
+;; delete them with additional -delete after the command
 
 (bind-key "C-c <f12>" 'rangi/byte-recompile)
 
