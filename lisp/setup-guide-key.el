1950efd589680a629d24d9eb40a58d33d8ce7768
Rangi Lin <rangiltw@gmail.com>
2014-09-06 00:56:04 +0800

    setup guide-key

--- lisp/setup-guide-key.el
+++ lisp/setup-guide-key.el
@@ -4,14 +4,12 @@
   :diminish guide-key-mode
   :init
   (progn
-    ;; ------------------------------ org-mode
     (defun rangi/guide-key-for-org-mode ()
-      (guide-key/add-local-guide-key-sequence "C-c")
-      (guide-key/add-local-guide-key-sequence "C-c C-x")
       (guide-key/add-local-highlight-command-regexp "org-"))
     (add-hook 'org-mode-hook 'rangi/guide-key-for-org-mode)
 
-    (setq guide-key/guide-key-sequence '("C-x r" "C-x 4"))
+    (setq guide-key/recursive-key-sequence-flag t)
+    (setq guide-key/guide-key-sequence '("C-c" "C-x"))
     (guide-key-mode 1)))
 
 (provide 'setup-guide-key)
