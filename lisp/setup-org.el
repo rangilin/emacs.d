4f1d8abdb4db122e6ad3e59c210c18243e922d00
Rangi Lin <rangiltw@gmail.com>
2014-09-06 00:48:45 +0800

    fix misplaced function call

--- lisp/setup-org.el
+++ lisp/setup-org.el
@@ -13,14 +13,10 @@
     (add-hook 'org-shiftup-final-hook 'windmove-up)
     (add-hook 'org-shiftleft-final-hook 'windmove-left)
     (add-hook 'org-shiftdown-final-hook 'windmove-down)
-    (add-hook 'org-shiftright-final-hook 'windmove-right))
-
+    (add-hook 'org-shiftright-final-hook 'windmove-right)
 
     (use-package gnuplot)
-
-
     (use-package org-plot
-      :bind (("C-M-g" . org-plot/gnuplot))))
-
+      :bind (("C-M-g" . org-plot/gnuplot)))))
 
 (provide 'setup-org)
