b7ca7c59428483fbe70a4375c6fd891bf263067c
Rangi Lin <rangiltw@gmail.com>
2014-09-18 00:03:35 +0800

    make recentf can apply sudo automatically

--- lisp/setup-recentf.el
+++ lisp/setup-recentf.el
@@ -27,6 +27,8 @@
   "Using ido to find recent file with specified function"
   (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
     (when file
-      (funcall find-file-function file))))
+      (if (file-writable-p file)
+          (funcall find-file-function file)
+        (funcall find-file-function (concat "/sudo::" file))))))
 
 (provide 'setup-recentf)
