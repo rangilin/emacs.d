ba76cc4c990c2f9ea07281ea5c11e9b4bdfaf186
Rangi Lin <rangiltw@gmail.com>
2014-09-22 22:06:36 +0800

    no idea know why web-mode reset undo tree keybinding

--- lisp/setup-web-mode.el
+++ lisp/setup-web-mode.el
@@ -13,6 +13,8 @@
     (setq-default web-mode-markup-indent-offset tab-width)
     (setq-default web-mode-css-indent-offset tab-width)
     (setq-default web-mode-code-indent-offset tab-width)
+
+    (bind-key "C-x u" 'undo-tree-visualize web-mode-map)
     (bind-key "C-/" 'web-mode-comment-or-uncomment web-mode-map)))
 
 (provide 'setup-web-mode)
