ba94675d42d7ef65557d5634ef1771a219e53963
Rangi Lin <rangiltw@gmail.com>
2014-09-27 22:40:19 +0800

    let php snippet works in web mode

--- lisp/setup-web-mode.el
+++ lisp/setup-web-mode.el
@@ -1,4 +1,6 @@
 (require 'use-package)
+(require 'yasnippet)
+
 (use-package web-mode
   :mode (("\\.phtml\\'" . web-mode)
          ("\\.[gj]sp\\'" . web-mode)
@@ -15,6 +17,13 @@
     (setq-default web-mode-code-indent-offset tab-width)
 
     (bind-key "C-x u" 'undo-tree-visualize web-mode-map)
-    (bind-key "C-/" 'web-mode-comment-or-uncomment web-mode-map)))
+    (bind-key "C-/" 'web-mode-comment-or-uncomment web-mode-map)
+
+    (defun rangi/web-mode-hook ()
+      (when (stringp buffer-file-name)
+          (cond ((string-match "\\.php\\'" buffer-file-name)
+                 (yas-activate-extra-mode 'php-mode)))))
+
+    (add-hook 'web-mode-hook 'rangi/web-mode-hook)))
 
 (provide 'setup-web-mode)
