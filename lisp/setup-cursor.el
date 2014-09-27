dfbf08750e4174f25a9d3b41a9e8b61746f14ad7
Rangi Lin <rangiltw@gmail.com>
2014-09-27 21:57:19 +0800

    add back smarter beginning of line function

--- lisp/setup-cursor.el
+++ lisp/setup-cursor.el
@@ -51,4 +51,13 @@
   :bind (("C-;" . ace-jump-mode)
          ("M-;" . ace-jump-mode-pop-mark)))
 
+;; ------------------------------ back to indentation or beginning
+;; http://www.emacswiki.org/emacs/BackToIndentationOrBeginning
+(defun rangi/back-to-indentation-or-beginning () (interactive)
+  "Back to indentation or beginning of current line"
+  (if (= (point) (progn (back-to-indentation) (point)))
+      (beginning-of-line)))
+
+(bind-key "C-a" 'rangi/back-to-indentation-or-beginning)
+
 (provide 'setup-cursor)
