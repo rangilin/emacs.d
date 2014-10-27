ddeb385dc7eb7579283a11f9fb82dd28209f7823
Rangi Lin <rangilin@otiga.com>
2014-10-27 10:22:05 +0800

    change backward kill word

--- lisp/setup-php.el
+++ lisp/setup-php.el
@@ -3,6 +3,6 @@
 (use-package php-mode
   :init
   (progn
-    (bind-key "C-M-h" 'rangi/backward-delete-word php-mode-map)))
+    (bind-key "C-M-h" 'backward-kill-word php-mode-map)))
 
 (provide 'setup-php)
