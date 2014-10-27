428659f774c3d0d3444c9967d2b07750804ff7b4
rangilin <rangiltw@gmail.com>
2014-10-28 00:43:06 +0800

    make delete trailing whitespace works php mode

--- lisp/setup-php.el
+++ lisp/setup-php.el
@@ -1,8 +1,16 @@
 (require 'use-package)
 
 (use-package php-mode
+  :mode "\\.php\\'"
   :init
   (progn
-    (bind-key "C-M-h" 'backward-kill-word php-mode-map)))
+    (setq-default php-mode-coding-style 'psr2))
+  :config
+  (progn
+    (defun rangi-setup-php-mode ()
+      (add-hook 'before-save-hook 'delete-trailing-whitespace)
+      (setq show-trailing-whitespace t))
 
+    (add-hook 'php-mode-psr2-hook 'rangi-setup-php-mode)
+    (bind-key "C-M-h" 'backward-kill-word php-mode-map)))
 (provide 'setup-php)
