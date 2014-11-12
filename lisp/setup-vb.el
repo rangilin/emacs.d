6f7dc82bd486055b1c0f5abfa84da8ddb5911090
Rangi Lin <rangilin@otiga.com>
2014-11-12 10:46:21 +0800

    truncate liens in vbnet mode

--- lisp/setup-vb.el
+++ lisp/setup-vb.el
@@ -2,6 +2,12 @@
 
 (use-package vbnet-mode
   :mode ("\\.vb$" . vbnet-mode)
-  :load-path "site-lisp/vbnet-mode")
+  :load-path "site-lisp/vbnet-mode"
+  :config
+  (progn
+    (defun rangi-setup-vbnet-mode ()
+      (toggle-truncate-lines))
+
+    (add-hook 'vbnet-mode-hook 'rangi-setup-vbnet-mode)))
 
 (provide 'setup-vb)
