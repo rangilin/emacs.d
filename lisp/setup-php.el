768aaa38835024cfeb5ae8170d0dec648b6385af
rangilin <rangiltw@gmail.com>
2014-11-23 15:59:04 +0800

    hot key in php mode

--- lisp/setup-php.el
+++ lisp/setup-php.el
@@ -3,7 +3,8 @@
 (use-package php-mode
   :config
   (progn
-    (setq-default php-mode-coding-style 'drupal)))
+    (setq-default php-mode-coding-style 'drupal)
+    (bind-key "C-M-h" 'backward-kill-word php-mode-map)))
 
 (use-package php-boris-minor-mode
   :config
