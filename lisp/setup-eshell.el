a4fc34c71a6bd5b49d813c1ed682d6caebccf030
rangilin <rangiltw@gmail.com>
2014-11-25 23:21:08 +0800

    config eshell

--- lisp/setup-eshell.el
+++ lisp/setup-eshell.el
@@ -4,7 +4,28 @@
 (use-package "eshell"
   :init
   (progn
-    (let ((rangi/eshell-dir (rangi/sub-gen-dir "eshell")))
-      (setq-default eshell-directory-name rangi/eshell-dir))))
+
+    (defmacro with-face (str &rest properties)
+      `(propertize ,str 'face (list ,@properties)))
+
+    (defun rangi-eshell-prompt ()
+      (concat
+       (propertize (format-time-string "[%H:%M:%S]" (current-time))
+                   'face font-lock-keyword-face)
+       (propertize (or (ignore-errors
+                         (format " [%s] " (vc-responsible-backend default-directory))) " ")
+                   'face font-lock-keyword-face)
+       (propertize (concat user-login-name "@" system-name)
+                   'face font-lock-constant-face)
+       " "
+       (propertize (abbreviate-file-name (eshell/pwd))
+                   'face font-lock-constant-face)
+       " \n"
+       (if (= (user-uid) 0)
+             (with-face " #" :foreground "red")
+         " ☞")
+       " "))
+
+    (setq-default eshell-prompt-function 'rangi-eshell-prompt)))
 
 (provide 'setup-eshell)
