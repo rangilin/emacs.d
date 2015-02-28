82d1f4ffa31afe4f997dd773be34a3c3145d7698
rangilin <rangiltw@gmail.com>
2015-02-28 16:50:52 +0800

    right option as control in mac

--- lisp/setup-mac.el
+++ lisp/setup-mac.el
@@ -1,6 +1,11 @@
 ;; Mac specified setup
 
+;; command as meta
 (setq-default mac-command-modifier 'meta)
 (setq-default mac-right-command-modifier 'meta)
+
+;; right option as ctrl
+(setq-default mac-right-option-modifier 'control)
+
 
 (provide 'setup-mac)
