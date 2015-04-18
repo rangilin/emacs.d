10aa9ea18fac2466e8690366ce9f19cfffdd5b38
rangilin <rangiltw@gmail.com>
2015-04-18 16:50:12 +0800

    improve copy current path

--- lisp/setup-copy.el
+++ lisp/setup-copy.el
@@ -1,20 +1,38 @@
-(require 'functions)
-(require 'use-package)
+(require 's)
 
-;; http://stackoverflow.com/a/9414763/554279
-(defun rangi-copy-current-file-path ()
+(defun rangi-copy-current-path (&optional prefix)
   "Copy file path of the current buffer"
-  (interactive)
-  (let ((filename (if (equal major-mode `dired-mode)
-                      default-directory
-                    (buffer-file-name))))
-    (if filename
+  (interactive "p")
+  (message "%d" (/ prefix 4))
+  (let ((path (rangi-current-path)))
+    (if path
         (progn
-          (kill-new filename)
-          (message "Copied buffer file name '%s' to the clipboard." filename))
-      (message "Not in a file, do nothing"))))
+          (let ((result (rangi-strip-path (/ prefix 4) path)))
+            (kill-new result)
+            (message "Copied '%s' to the clipboard." result)))
+      (message "Not in a file or directory, do nothing"))))
 
-(bind-key "C-c c f" 'rangi-copy-current-file-path)
 
+(defun rangi-current-path ()
+  "Return full path of current buffer"
+  (if (equal major-mode `dired-mode)
+      default-directory
+    (buffer-file-name)))
+
+(defun rangi-strip-path (index path)
+  "Strip path according to index, 1 will return last element of the path,
+4 return parent directory, otherwise return path itself"
+  (cond
+   ((equal 1 index) (rangi-path-base path))
+   ((equal 4 index) (file-name-directory (directory-file-name path)))
+   (t path)))
+
+(defun rangi-path-base (path)
+  "Return last element of path of PATH"
+  (let* ((parent-dir (file-name-directory (directory-file-name path))))
+    (s-chop-prefix parent-dir (directory-file-name path))))
+
+
+(bind-key "C-c c f" 'rangi-copy-current-path)
 
 (provide 'setup-copy)
