042deb2cbd419f4eef3312eb41de8753bdad46b3
rangilin <rangiltw@gmail.com>
2015-04-18 15:15:57 +0800

    organize css

--- lisp/setup-css.el
+++ lisp/setup-css.el
@@ -3,7 +3,6 @@
 (use-package css-mode
   :mode ("\\.css$" . css-mode)
   :config
-  (progn
-    (setq-default css-indent-offset tab-width)))
+  (setq-default css-indent-offset tab-width))
 
 (provide 'setup-css)
