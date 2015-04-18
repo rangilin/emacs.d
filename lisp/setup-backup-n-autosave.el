e9b36926efe559dd6e21f0338e612b2f5882c6d0
rangilin <rangiltw@gmail.com>
2015-04-18 13:06:45 +0800

    organize setup-backup-n-autosave.el

--- lisp/setup-backup-n-autosave.el
+++ lisp/setup-backup-n-autosave.el
@@ -4,14 +4,15 @@
 (setq-default backup-by-copying t)
 
 ;; put backup files in gen dir
-(let ((rangi/backup-dir (rangi-sub-gen-dir "backup")))
-  (setq backup-directory-alist `((".*" . ,rangi/backup-dir))))
+(let ((dir (rangi-sub-gen-dir "backup")))
+  (setq backup-directory-alist `((".*" . ,dir))))
 
 ;; ------------------------------ auto-save
 ;; put auto save files in gen dir
-(let ((rangi/autosave-dir (rangi-sub-gen-dir "autosave")))
-  (setq auto-save-file-name-transforms `((".*" ,rangi/autosave-dir t)))
-  ;; prevent `auto-save-list' empty dir created
-  (setq auto-save-list-file-prefix nil))
+(let ((dir (rangi-sub-gen-dir "autosave")))
+  (setq auto-save-file-name-transforms `((".*" ,dir t))))
+
+;; prevent `auto-save-list' empty dir created
+(setq auto-save-list-file-prefix nil)
 
 (provide 'setup-backup-n-autosave)
