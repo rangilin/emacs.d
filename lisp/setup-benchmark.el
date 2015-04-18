c8277b2300388803a1bb6dfa0cdd79f6bee28999
rangilin <rangiltw@gmail.com>
2015-04-18 12:39:12 +0800

    add el for benchmark emacs

---
+++ lisp/setup-benchmark.el
@@ -0,0 +1,7 @@
+(add-hook 'after-init-hook 'rangi-show-init-time)
+
+(defun rangi-show-init-time ()
+  "Show how much time init.el took to load"
+  (message "init.el took %.2f seconds to load" (float-time (time-subtract after-init-time before-init-time))))
+
+(provide 'setup-benchmark)
