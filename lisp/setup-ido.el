6401fb8bee768d2c0febf2098227c4950235d34a
rangilin <rangiltw@gmail.com>
2015-04-19 19:32:33 +0800

    organize ido

--- lisp/setup-ido.el
+++ lisp/setup-ido.el
@@ -3,45 +3,44 @@
 
 (use-package ido
   :init
-  (progn
-    (ido-mode 1))
+  (ido-mode 1)
+
   :config
-  (progn
-    (use-package ido-ubiquitous
-      :init (ido-ubiquitous-mode 1))
-    (use-package ido-vertical-mode
-      :init
-      (progn
-        (ido-vertical-mode)
-        (setq-default ido-vertical-define-keys nil)))
-    (use-package flx-ido
-      :init (flx-ido-mode 1)
-      :config
-      (progn
-        ;; disable ido faces so can see flx highlights
-        (setq ido-use-faces nil)))
+  (use-package ido-ubiquitous :init (ido-ubiquitous-mode 1))
 
-    (setq ido-enable-flex-matching t)
-    (setq ido-create-new-buffer 'always)
-    (setq ido-case-fold t)
-    (setq ido-everywhere t)
+  (use-package ido-vertical-mode
+    :init
+    (ido-vertical-mode)
+    (setq-default ido-vertical-define-keys nil))
 
-    ;; make ido find file try sudo automatically
-    (defadvice ido-find-file (after find-file-sudo activate)
-      "Find file as root if necessary."
-      (unless (and buffer-file-name
-                   (file-writable-p buffer-file-name))
-        (find-alternate-file (concat "/sudo::" buffer-file-name))))
+  (use-package flx-ido
+    :init (flx-ido-mode 1)
+    :config
+    ;; disable ido faces so can see flx highlights
+    (setq ido-use-faces nil))
 
-    (defadvice ido-find-file-other-window (after find-file-other-window-sudo activate)
-      "Find file as root if necessary."
-      (unless (and buffer-file-name
-                   (file-writable-p buffer-file-name))
-        (find-alternate-file (concat "/sudo::" buffer-file-name))))
+  (setq ido-enable-flex-matching t)
+  (setq ido-create-new-buffer 'always)
+  (setq ido-case-fold t)
+  (setq ido-everywhere t)
 
-    (defadvice ido-find-file-other-frame (after find-file-other-frame-sudo activate)
-      "Find file as root if necessary."
-      (unless (and buffer-file-name
-                   (file-writable-p buffer-file-name))
-        (find-alternate-file (concat "/sudo::" buffer-file-name))))))
+  ;; make ido find file try sudo automatically
+  (defadvice ido-find-file (after find-file-sudo activate)
+    "Find file as root if necessary."
+    (unless (and buffer-file-name
+                 (file-writable-p buffer-file-name))
+      (find-alternate-file (concat "/sudo::" buffer-file-name))))
+
+  (defadvice ido-find-file-other-window (after find-file-other-window-sudo activate)
+    "Find file as root if necessary."
+    (unless (and buffer-file-name
+                 (file-writable-p buffer-file-name))
+      (find-alternate-file (concat "/sudo::" buffer-file-name))))
+
+  (defadvice ido-find-file-other-frame (after find-file-other-frame-sudo activate)
+    "Find file as root if necessary."
+    (unless (and buffer-file-name
+                 (file-writable-p buffer-file-name))
+      (find-alternate-file (concat "/sudo::" buffer-file-name)))))
+
 (provide 'setup-ido)
