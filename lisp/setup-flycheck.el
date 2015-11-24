4ffbccb9f53a6dbcf4fccb09e8ed4aa9c08191a2
rangilin <rangiltw@gmail.com>
2015-11-25 00:40:28 +0800

    change flycheck prefix key

--- lisp/setup-flycheck.el
+++ lisp/setup-flycheck.el
@@ -2,11 +2,15 @@
 
 (use-package flycheck
   :config
-  (progn
-    (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
-    (setq flycheck-emacs-lisp-load-path load-path)
-    (setq-default flycheck-emacs-lisp-load-path load-path)
-    (add-hook 'after-init-hook 'global-flycheck-mode)))
+  (define-key flycheck-mode-map flycheck-keymap-prefix nil)
+    (setq flycheck-keymap-prefix (kbd "C-c f"))
+    (define-key flycheck-mode-map flycheck-keymap-prefix
+      flycheck-command-map)
+
+  (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
+  (setq flycheck-emacs-lisp-load-path load-path)
+  (setq-default flycheck-emacs-lisp-load-path load-path)
+  (add-hook 'after-init-hook 'global-flycheck-mode))
 
 (use-package flycheck-cask
   :config (add-hook 'flycheck-mode-hook 'flycheck-cask-setup))
