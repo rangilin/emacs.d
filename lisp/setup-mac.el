070a3cccf6cfd118ea1a2bafde5d486fb85001cd
rangilin <rangiltw@gmail.com>
2016-11-20 10:15:34 +0800

    change how mac is configured

--- lisp/setup-mac.el
+++ lisp/setup-mac.el
@@ -1,4 +1,10 @@
 ;; Mac specified setup
 
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
+(when window-system
+  (set-frame-size (selected-frame) 120 42)
+  (set-frame-position (selected-frame) 140 25))
 
 (provide 'setup-mac)
