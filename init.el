f3097d4b56791d629bfb6192cd882da608784270
rangilin <rangiltw@gmail.com>
2017-03-08 23:36:49 +0800

    load use-package first

---
+++ init.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path (expand-file-name "site-lisp/use-package" user-emacs-directory))
+(require 'use-package)
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
