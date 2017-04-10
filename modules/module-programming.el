8ac4d203f8a3d09d37618ec3a3702d0ff29a5d60
rangilin <rangiltw@gmail.com>
2017-04-11 00:35:44 +0800

    PHP mode, company mode

--- modules/module-programming.el
+++ modules/module-programming.el
@@ -1,13 +1,22 @@
 (defun rl-init-module-programming ()
   (rl--set-up-flycheck)
+  (rl--set-up-auto-complete)
   (rl--set-up-comment)
   (rl--set-up-php))
+
+
+(defun rl--set-up-auto-complete ()
+  (use-package company
+    :diminish company-mode
+    :ensure t))
 
 
 (defun rl--set-up-flycheck ()
   (use-package flycheck
     :ensure t
     :diminish flycheck-mode
+    :init
+    (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
     :config (global-flycheck-mode)))
 
 
@@ -21,7 +30,23 @@
   "Set up PHP mode."
   (use-package php-mode
     :mode "\\.php\\'"
-    :ensure t))
+    :ensure t
+    :config
+    (add-hook 'php-mode-hook 'rl--php-mode-hook))
+  (use-package ac-php :ensure t))
+
+
+(defun rl--php-mode-hook ()
+  "My PHP mode hook."
+  (company-mode t)
+  (ac-php-core-eldoc-setup)
+  (add-to-list 'company-backends 'company-ac-php-backend))
+
+
+(defun rl--set-up-eldoc ()
+  "Set up eldoc mode."
+  (use-package eldoc
+    :diminish eldoc-mode))
 
 
 
