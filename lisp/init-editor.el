4402a1e31417417e3a08afb8d5e66cacc08fd8d8
rangilin <rangiltw@gmail.com>
2018-05-01 22:09:58 +0800

    handle beginning of line

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -98,7 +98,17 @@
 ;; move cursor to top or bottom of the buffer when it cannot be scrolled anymore
 (setq-default scroll-error-top-bottom t)
 
+;; smarter back-to-beginning behavior
+;; http://www.emacswiki.org/emacs/BackToIndentationOrBeginning
+(defun rangi-back-to-indentation-or-beginning ()
+  "Back to indentation or beginning of current line"
+  (interactive "^")
+  (if (bound-and-true-p visual-line-mode)
+      (beginning-of-visual-line)
+    (when (= (point) (progn (back-to-indentation) (point)))
+      (beginning-of-line))))
 
+(global-set-key (kbd "C-a") 'rangi-back-to-indentation-or-beginning)
 
 
 
