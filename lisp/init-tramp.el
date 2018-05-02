196f3ff2b6ac36ee06dac82bc6df658971ce8b14
rangilin <rangiltw@gmail.com>
2018-05-02 21:53:17 +0800

    handle backup behavior in remote file

--- lisp/init-tramp.el
+++ lisp/init-tramp.el
@@ -6,6 +6,9 @@
 ;; assign tramp file location
 (setq-default tramp-persistency-file-name(expand-file-name "tramp" rangi-generated-files-directory))
 
+;; backup remote file in local machine instead of remote server
+(setq tramp-backup-directory-alist backup-directory-alist)
+
 ;; for debugging tramp
 ;; (setq-default tramp-debug-buffer t)
 ;; (setq-default tramp-verbose 10)
