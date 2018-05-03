1fbd20931d13bcfc2aa91f7088eb297df9330ff4
rangilin <rangiltw@gmail.com>
2018-05-04 07:37:10 +0800

    increase length of the name column in ibuffer

--- lisp/init-buffer.el
+++ lisp/init-buffer.el
@@ -59,7 +59,7 @@
 ;; define ibuffer columns
 (setq-default ibuffer-formats
               '((mark modified read-only
-                      " " (name 24 24 :left :elide)
+                      " " (name 36 36 :left :elide)
                       " " (readable-size 9 -1 :right)
                       " " (mode 16 16 :left :elide)
                       " " filename-and-process)))
