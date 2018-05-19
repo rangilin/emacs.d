5bb509960e1bc8be4cda06493a78b29c61a7ee4c
rangilin <rangiltw@gmail.com>
2018-05-19 15:15:42 +0800

    remove duplicate initialize

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,16 +1,18 @@
 (require 'package)
 
-;;; install into separate directories for each Emacs version, to prevent bytecode incompatibility
+;; install into separate directories for each Emacs version, to prevent bytecode incompatibility
 (let ((versioned-package-dir
        (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version) rangi-generated-files-directory)))
   (setq package-user-dir versioned-package-dir))
 
 
-;; offical melpa mirror repository, just in case we need it
-;; (add-to-list 'package-archives '("melpa" . "https://www.mirrorservice.org/sites/melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "https://www.mirrorservice.org/sites/melpa.org/packages/") t)
 (add-to-list 'package-archives '("org"   . "http://orgmode.org/elpa/") t)
+
+(setq package-enable-at-startup nil)
 (package-initialize)
+
 
 
 (defun require-package (package &optional min-version no-refresh)
@@ -29,9 +31,6 @@ re-downloaded in order to locate PACKAGE."
         (require-package package min-version t)))))
 
 
-(setq package-enable-at-startup nil)
-(package-initialize)
-
 
 ;; remind me to update package on 1st & 15th every month on start up
 (defun rangi-package-update-reminder ()
@@ -40,7 +39,6 @@ re-downloaded in order to locate PACKAGE."
             (string= (format-time-string "%d") "15"))
             (when (y-or-n-p "It has been a while since last reminder, do you want to check package update ?")
               (package-list-packages))))
-
 (add-hook 'emacs-startup-hook 'rangi-package-update-reminder)
 
 
