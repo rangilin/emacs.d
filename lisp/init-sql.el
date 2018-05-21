c5598fc821ee01acba4e585ed636f7a81e15ec55
rangilin <rangiltw@gmail.com>
2018-05-21 22:17:49 +0800

    sql indentation

--- lisp/init-sql.el
+++ lisp/init-sql.el
@@ -1,3 +1,27 @@
+;;;; SQL indentation
+(require 'sql-indent)
+
+;; enable indentation mode in SQL mode
+(add-hook 'sql-mode-hook (lambda () (sqlind-minor-mode)))
+
+;; customize indentation
+(defvar rangi-sql-indentation-offsets-alist
+  `((select-clause 0)
+    (insert-clause 0)
+    (delete-clause 0)
+    (update-clause 0)
+    ,@sqlind-default-indentation-offsets-alist))
+
+(add-hook 'sqlind-minor-mode-hook
+          (lambda ()
+            (setq sqlind-indentation-offsets-alist
+                  rangi-sql-indentation-offsets-alist)))
+
+
+
+;;;; SQL interactive
+
+;;  set mysql SQLi buffer
 (defun rangi-sql-set-mysql-sqli-buffer()
   "Set MySQL SQLi buffer for a buffer in SQL mode"
   (interactive)
