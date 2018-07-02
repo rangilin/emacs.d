f2ae999d5f5ad652addadb0dc08564f7c3c6a192
rangilin <rangiltw@gmail.com>
2018-07-02 12:13:59 +0800

    add function to randomize region

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -266,5 +266,18 @@
 
 
 
+;;;; randomize region
+(defun rangi-randomize-region (beg end)
+  "Randomize lines in region from BEG to END."
+  (interactive "*r")
+  (let ((lines (split-string
+                (delete-and-extract-region beg end) "\n")))
+    (when (string-equal "" (car (last lines 1)))
+      (setq lines (butlast lines 1)))
+    (apply 'insert
+           (mapcar 'cdr
+                   (sort (mapcar (lambda (x) (cons (random) (concat x "\n"))) lines)
+                         (lambda (a b) (< (car a) (car b))))))))
+
 
 (provide 'init-editor)
