5f13ddf23706af9efe3d48ca9c035a1447adb067
Rangi Lin <me@rangilin.idv.tw>
2018-08-24 21:36:11 +0800

    fix breaking changes

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,5 +1,5 @@
 (require-package 'projectile)
-(require-package 'counsel-projectile)
+(require 'projectile)
 
 (setq-default projectile-mode-line
               '(:eval (format " P[%s]" (projectile-project-name))))
@@ -8,6 +8,11 @@
 (setq projectile-cache-file (expand-file-name "projectile.cache" rangi-generated-files-directory))
 (setq projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld" rangi-generated-files-directory))
 
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+
+(require-package 'counsel-projectile)
 (counsel-projectile-mode)
 
 (provide 'init-project)
