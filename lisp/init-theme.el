da2939c41d01ed2c986086786b51914b0ea42a02
Rangi Lin <me@rangilin.idv.tw>
2018-08-28 12:09:53 +0800

    use solarized theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,30 +1,32 @@
 ;; install themes
 (require-package 'color-theme-sanityinc-tomorrow)
-(require-package 'dracula-theme)
+(require-package 'solarized-theme)
+
+
+(defun rangi-load-solarized-dark-theme ()
+  (setq solarized-use-variable-pitch nil)
+  (setq solarized-scale-org-headlines nil)
+  (setq x-underline-at-descent-line t)
+  (load-theme 'solarized-dark t))
 
 
 ;; load theme
-(load-theme 'dracula t)
-
+(rangi-load-solarized-dark-theme)
 
 
 ;; some customization
 (let ((fg (face-attribute 'default :foreground))
       (bg (face-attribute 'default :background)))
 
-  ;; increase mode line height
+  ;; ;; increase mode line height
   (set-face-attribute 'mode-line nil :box `(:line-width 5 :color ,(face-attribute 'mode-line :background)))
   (set-face-attribute 'mode-line-inactive nil :box `(:line-width 5 :color ,(face-attribute 'mode-line-inactive :background)))
 
   ;; make ace window faces clear
   (set-face-attribute 'aw-leading-char-face nil :foreground "red" :weight 'extra-bold :height 300)
 
-  ;; make fringe looks like part of the buffer
+  ;; ;; make fringe looks like part of the buffer
   (set-face-background 'fringe bg)
-
-  ;; increase mode line height
-  (set-face-attribute 'mode-line nil :box `(:line-width 5 :color ,(face-attribute 'mode-line :background)))
-  (set-face-attribute 'mode-line-inactive nil :box `(:line-width 5 :color ,(face-attribute 'mode-line-inactive :background)))
 
   (with-eval-after-load 'org
     (set-face-attribute 'org-level-1 nil :height 1.0)
