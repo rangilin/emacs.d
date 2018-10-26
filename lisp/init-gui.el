bf7b7ece17a7e447ad8ae26121c67c1288601e3d
Rangi Lin <me@rangilin.idv.tw>
2018-10-26 22:58:55 +0800

    add document for keys in arrangement-hydra

--- lisp/init-gui.el
+++ lisp/init-gui.el
@@ -67,23 +67,31 @@
 (require-package 'buffer-move)
 (require-package 'transpose-frame)
 
-(defhydra hydra-fwb-arrangement (:color amaranth :hint nil)
-  "Arranging window & buffer & frame ..."
+(defhydra hydra-fwb-arrangement (:color amaranth :hint none)
+  "
+  Arranging window & buffer & frame ... (_q_: Quit)
+
+  ___: Shrink window horizontally     _j_: Move window down    _J_: Move buffer down    _d_: Rotate frame clockwise ↻
+  _+_: Enlarge window horizontally    _k_: Move window up      _K_: Move buffer up      _a_: Rotate frame anticlockwise ↺
+  _-_: Shrink window                  _l_: Move window right   _L_: Move buffer right   _w_: Flip frame ⇕
+  _=_: Enlarge window                 _h_: Move window left    _H_: Move buffer left    _s_: Flop frame ⟺
+  ^ ^                                 ^ ^                      ^ ^                      _e_: Transpose frame
+  "
 
   ("_" shrink-window-horizontally)
   ("+" enlarge-window-horizontally)
   ("-" shrink-window)
   ("=" enlarge-window)
 
-  ("J" buf-move-down)
-  ("K" buf-move-up)
-  ("L" buf-move-right)
-  ("H" buf-move-left)
-
   ("j" windmove-down)
   ("k" windmove-up)
   ("l" windmove-right)
   ("h" windmove-left)
+
+  ("J" buf-move-down)
+  ("K" buf-move-up)
+  ("L" buf-move-right)
+  ("H" buf-move-left)
 
   ("d" rotate-frame-clockwise)
   ("a" rotate-frame-anticlockwise)
