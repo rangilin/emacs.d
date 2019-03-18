978d57014b8cc27c9d79641b4df1a9cf8c29ca4c
Rangi Lin <me@rangilin.idv.tw>
2019-03-18 23:31:12 +0800

    move magit transient files

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,5 +1,9 @@
 (require-package 'magit)
 
+(setq-default transient-levels-file (locate-user-emacs-file (convert-standard-filename "gen/transient/levels.el")))
+(setq-default transient-values-file (locate-user-emacs-file (convert-standard-filename "gen/transient/values.el")))
+(setq-default transient-history-file (locate-user-emacs-file (convert-standard-filename "gen/transient/history.el")))
+
 (with-eval-after-load 'magit
   (setq-default magit-diff-refine-hunk t))
 
