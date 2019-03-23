389c1b90ec07ebcd5e3ace0fd72f4e74fda86fdb
Rangi Lin <me@rangilin.idv.tw>
2019-03-23 22:34:44 +0800

    select marks

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -216,6 +216,40 @@
   (bind-key "s-G" 'rangi-active-cursor-jump))
 
 
+;; select marks
+(defun marker-is-point-p (marker)
+  "test if marker is current point"
+  (and (eq (marker-buffer marker) (current-buffer))
+       (= (marker-position marker) (point))))
+
+(defun push-mark-maybe ()
+  "push mark onto `global-mark-ring' if mark head or tail is not current location"
+  (if (not global-mark-ring) (error "global-mark-ring empty")
+    (unless (or (marker-is-point-p (car global-mark-ring))
+                (marker-is-point-p (car (reverse global-mark-ring))))
+      (push-mark))))
+
+(defun backward-global-mark ()
+  "use `pop-global-mark', pushing current point if not on ring."
+  (interactive)
+  (push-mark-maybe)
+  (when (marker-is-point-p (car global-mark-ring))
+    (call-interactively 'pop-global-mark))
+  (call-interactively 'pop-global-mark))
+
+(defun forward-global-mark ()
+  "hack `pop-global-mark' to go in reverse, pushing current point if not on ring."
+  (interactive)
+  (push-mark-maybe)
+  (setq global-mark-ring (nreverse global-mark-ring))
+  (when (marker-is-point-p (car global-mark-ring))
+    (call-interactively 'pop-global-mark))
+  (call-interactively 'pop-global-mark)
+  (setq global-mark-ring (nreverse global-mark-ring)))
+
+(global-set-key (kbd "s-,") 'backward-global-mark)
+(global-set-key (kbd "s-.") 'forward-global-mark)
+
 
 ;;
 ;; Clipboard & Kill Ring
