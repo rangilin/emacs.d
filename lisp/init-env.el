07051800adad7b94cbc205521d0a5b3e692a3389
Rangi Lin <me@rangilin.idv.tw>
2019-03-24 17:55:53 +0800

    add info

--- lisp/init-env.el
+++ lisp/init-env.el
@@ -1,3 +1,7 @@
+;; basic info
+(setq user-full-name "Rangi Lin")
+(setq user-mail-address (s-join "" '("inb" "ox@me.r" "angil" "in.i" "dv.tw")))
+
 ;; set up path info from shell environment to emacs
 (use-package exec-path-from-shell
   :config
