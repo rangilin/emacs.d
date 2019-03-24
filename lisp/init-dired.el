cccc0a390af1699ef0f8054e09d7349a47f69e50
Rangi Lin <me@rangilin.idv.tw>
2019-03-24 21:52:21 +0800

    don't confirm some dired actions

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -9,12 +9,19 @@
 ;; open home folder
 (global-set-key (kbd "<S-f12>") (lambda () (interactive) (dired-other-window "~/")))
 
-
 ;; Prefer g-prefixed coreutils version of standard utilities when available
 (setq insert-directory-program (executable-find "gls"))
 
 ;; sort dired list
 (setq dired-listing-switches "-aBhl  --group-directories-first")
+
+;; don't ask about killing buffer visiting file
+(setq dired-clean-confirm-killing-deleted-buffers t)
+
+;; stop asking about recurisve actions
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+
 
 
 (provide 'init-dired)
