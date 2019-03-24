cd962197f24941e214de4e8c06b6ff2c00713384
Rangi Lin <me@rangilin.idv.tw>
2019-03-24 17:56:02 +0800

    add function to start a new emacs instance

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -3,9 +3,15 @@
   :delight
   (emacs-lisp-mode "Elisp" :major))
 
+
 (use-package eldoc
   :delight eldoc-mode)
 
+
+(defun rangi-new-emacs ()
+  "Create a new emacs instance, mostly for testing purpose."
+  (interactive)
+  (start-process "Emacs" nil (executable-find "/Applications/Emacs.app/Contents/MacOS/Emacs")))
 
 
 (provide 'init-emacs-lisp)
