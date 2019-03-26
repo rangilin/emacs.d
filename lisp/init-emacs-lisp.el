8f714723679d505261f4b9b09da4e52b3d9c0156
Rangi Lin <me@rangilin.idv.tw>
2019-03-26 23:44:02 +0800

    support arguments for restarting Emacs

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -8,10 +8,17 @@
   :delight eldoc-mode)
 
 
-(defun rangi-new-emacs ()
-  "Create a new emacs instance, mostly for testing purpose."
-  (interactive)
-  (start-process "Emacs" nil (executable-find "/Applications/Emacs.app/Contents/MacOS/Emacs")))
+;; restart Emacs
+(defun rangi-new-emacs (&optional prefix)
+  "Create a new Emacs instance, mostly for testing purpose"
+  (interactive "p")
+  (let ((name "Emacs")
+        (command "/Applications/Emacs.app/Contents/MacOS/Emacs")
+        (arg (cond ((equal prefix 4) "--debug-init")
+                   ((equal prefix 16) "-Q")
+                   (t ""))))
+    (start-process name nil (executable-find command) arg)))
 
+(global-set-key (kbd "C-c e r") 'rangi-new-emacs)
 
 (provide 'init-emacs-lisp)
