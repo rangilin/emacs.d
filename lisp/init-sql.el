617fa9cf924375773a2c3eb006c43ea43cf63965
Rangi Lin <me@rangilin.idv.tw>
2019-03-27 22:09:35 +0800

    set up sql interactive mode

--- lisp/init-sql.el
+++ lisp/init-sql.el
@@ -1,5 +1,18 @@
+(use-package sql
+  :config
+  ;;  set mysql SQL interactive buffer
+  (defun rangi-set-mysql-sqli-buffer()
+    "Set MySQL SQLi buffer for a buffer in SQL mode"
+    (interactive)
+    (sql-set-product "mysql")
+    (sql-set-sqli-buffer))
+  (add-hook 'sql-mode-hook #'rangi-set-mysql-sqli-buffer))
+
+
 (use-package sql-indent
+  :delight
   :config
   (add-hook 'sql-mode-hook 'sqlind-minor-mode))
+
 
 (provide 'init-sql)
