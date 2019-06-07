ef1ddaf6408e548f5010e45f999b9c926cd437c5
Rangi Lin <me@rangilin.idv.tw>
2019-06-07 19:38:57 +0800

    use enh-ruby-mode, some more snippets

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -1,6 +1,11 @@
+(use-package enh-ruby-mode
+  :mode (("\\.rb$" . enh-ruby-mode))
+  :config
+  (setq enh-ruby-deep-indent-paren nil))
+
 (use-package inf-ruby
   :config
-  (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode))
+  (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode))
 
 (use-package rvm
   :config
