036aecaf406379b0605413f544a5955098be046b
Rangi Lin <me@rangilin.idv.tw>
2019-06-07 11:55:25 +0800

    open ERB file in web mode

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -1,7 +1,8 @@
 ;; web-mode
 (use-package web-mode
   :mode (("\\.html\\'" . web-mode)
-         ("\\.hbs\\'" . web-mode))
+         ("\\.hbs\\'" . web-mode)
+         ("\\.html.erb\\'" . web-mode))
   :config
   (setq-default web-mode-markup-indent-offset 4)
   (setq-default web-mode-css-indent-offset 4)
