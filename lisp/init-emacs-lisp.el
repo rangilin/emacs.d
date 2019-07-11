3b8ad40ca904b30fe14abdee8e1c196cd01a5052
Rangi Lin <me@rangilin.idv.tw>
2019-07-11 16:21:31 +0800

    add eval and replace

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -8,6 +8,18 @@
   :delight eldoc-mode)
 
 
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+(bind-key "C-c C-e" 'eval-and-replace)
+
 ;; restart Emacs
 (defun rangi-new-emacs (&optional prefix)
   "Create a new Emacs instance, mostly for testing purpose"
