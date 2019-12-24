3424f27c43480d12c3bed3f0416b795d14fe4194
Rangi Lin <me@rangilin.idv.tw>
2019-12-24 11:22:12 +0800

    Allow to input buffer name before create it

--- lisp/init-buffer.el
+++ lisp/init-buffer.el
@@ -3,7 +3,7 @@
 
 (defun rangi-new-buffer ()
   (interactive)
-  (switch-to-buffer (generate-new-buffer "untitled")))
+  (switch-to-buffer (generate-new-buffer (read-string "Enter buffer name: " "untitled"))))
 
 (bind-key "M-s-b" 'rangi-new-buffer)
 
