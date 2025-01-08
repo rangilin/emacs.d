02577f67dfdbfb34d4c79044246262848e554851
Rangi Lin <me@rangilin.idv.tw>
2025-01-08 17:33:30 +0800

    remove markdown command since I don't export

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -4,13 +4,7 @@
          ("\\.md\\'"       . markdown-mode)
          ("README\\.md\\'" . gfm-mode))
   :init
-  (add-hook 'markdown-mode-hook (lambda () (turn-on-flyspell)))
-  (setq markdown-command
-        (concat "/usr/local/bin/pandoc -s"
-                " --quiet"
-                " -c ~/Documents/pandoc/pandoc.css"
-                " -f markdown"
-                " -t html")))
+  (add-hook 'markdown-mode-hook (lambda () (turn-on-flyspell))))
 
 
 (provide 'init-markdown)
