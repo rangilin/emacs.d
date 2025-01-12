d99436176e8870d1c2ea1edc01d8b0497ba5890b
Rangi Lin <me@rangilin.idv.tw>
2025-01-12 23:01:14 +0800

    gofmt before save

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,6 +1,7 @@
 (use-package go-mode
   :mode "\\.go\\'"
   :interpreter "go"
-  :hook ((go-mode . eglot-ensure)))
+  :hook ((go-mode . eglot-ensure)
+         (before-save . gofmt-before-save)))
 
 (provide 'init-go)
