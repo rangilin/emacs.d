f176f08657d973519aa3545596aa854c94638cbd
Rangi Lin <me@rangilin.idv.tw>
2025-01-14 23:48:35 +0800

    use ag and magit with project.el

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,10 +1,30 @@
-
 (use-package project
-  :bind-keymap (("s-p" . project-prefix-map) ; add other keys to access project key map
+  :bind-keymap (("s-p" . project-prefix-map)
                 ("C-c p" . project-prefix-map))
+  :bind (:map project-prefix-map
+              ("s" . rangi-project-ag)
+              ("v" . rangi-project-vc-dir))
   :config
   (setq project-list-file (expand-file-name "projects" rangi-emacs-cache-directory)))
 
+
+(defun rangi-project-ag ()
+  "use ag search current files in project"
+  (interactive)
+  (let ((p (project-current)))
+    (if p
+        (counsel-ag nil (project-root p))
+      (message "Currently not in a project"))))
+
+(defun rangi-project-vc-dir ()
+  "open magit or vc-dir"
+  (interactive)
+  (let ((p (project-current)))
+    (cond ((and p (string-equal "Git" (nth 1 p)))
+           (magit-status))
+          (p
+           (vc-dir (project-root p)))
+          (t (message "Currently not in a project")))))
 
 
 (provide 'init-project)
