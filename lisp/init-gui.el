aa310e9ce6b0a723b8af96e694edcf9fee870b48
Rangi Lin <me@rangilin.idv.tw>
2025-01-14 22:33:32 +0800

    change default font size

--- lisp/init-gui.el
+++ lisp/init-gui.el
@@ -217,21 +217,21 @@
 (defvar rangi-font-alist-roboto-and-noto-sans
   '((english-font . "Roboto Mono")
     (cjk-font . "Noto Sans Mono CJK TC")
-    (default-size-pair . (12 . 14))
+    (default-size-pair . (14 . 16))
     (size-pairs . ((10 . 12) (12 . 14) (14 . 16) (15 . 18) (16 . 20) (18 . 22) (20 . 24)
                    (22 . 26) (24 . 28) (26 . 32) (28 . 34) (30 . 36) (34 . 40) (36 . 44)))))
 
 (defvar rangi-font-alist-monaco-and-noto-sans
   '((english-font . "Monaco")
     (cjk-font . "Noto Sans Mono CJK TC")
-    (default-size-pair . (12 . 14))
+    (default-size-pair . (14 . 16))
     (size-pairs . ((10 . 12) (12 . 14) (14 . 16) (15 . 18) (16 . 20) (18 . 22) (20 . 24)
                    (22 . 26) (24 . 28) (26 . 32) (28 . 34) (30 . 36) (34 . 40) (36 . 44)))))
 
 (defvar rangi-font-alist-meslo-and-noto-sans
   '((english-font . "MesloLGS NF")
     (cjk-font . "Noto Sans Mono CJK TC")
-    (default-size-pair . (12 . 14))
+    (default-size-pair . (14 . 16))
     (size-pairs . ((10 . 12) (12 . 14) (14 . 16) (15 . 18) (16 . 20) (18 . 22) (20 . 24)
                    (22 . 26) (24 . 28) (26 . 32) (28 . 34) (30 . 36) (34 . 40) (36 . 44)))))
 
