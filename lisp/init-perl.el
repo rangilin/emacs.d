4cb43278699d1f2a0d9c0fe6e43996e91c71875d
Rangi Lin <me@rangilin.idv.tw>
2025-01-14 12:11:44 +0800

    add perldoc stuff

--- lisp/init-perl.el
+++ lisp/init-perl.el
@@ -1,6 +1,15 @@
+(defvar rangi-cperl-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "d" 'cperl-perldoc-at-point)
+    (define-key map "D" 'cperl-perldoc)
+    map)
+  "My keymap in cperl mode")
+
 (use-package cperl-mode
   :mode "\\.pl\\'"
   :interpreter "perl"
+  :bind-keymap (("s-m" . rangi-cperl-mode-map)
+                ("C-c m" . rangi-cperl-mode-map))
   :config
   (setq cperl-invalid-face nil)
   :hook ((cperl-mode . eglot-ensure)))
