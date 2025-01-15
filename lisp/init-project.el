ec3eb4a3cb580b8816fb2cd10a707bd48d586cad
Rangi Lin <me@rangilin.idv.tw>
2025-01-15 16:55:45 +0800

    add project find file in other window

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -3,6 +3,7 @@
                 ("C-c p" . project-prefix-map))
   :bind (:map project-prefix-map
               ("s" . rangi-project-ag)
+              ("4 f" . rangi-project-find-file-other-window)
               ("v" . rangi-project-vc-dir))
   :config
   (setq project-list-file (expand-file-name "projects" rangi-emacs-cache-directory)))
@@ -25,6 +26,13 @@
           (p
            (vc-dir (project-root p)))
           (t (message "Currently not in a project")))))
+
+(defun rangi-project-find-file-other-window ()
+  "find project file but open it in other window"
+  (interactive)
+  (split-window-right)
+  (other-window 1)
+  (project-find-file))
 
 
 (provide 'init-project)
