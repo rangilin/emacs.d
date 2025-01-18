dc0805539dfb498cc0453fac4638229ae80dd195
Rangi Lin <me@rangilin.idv.tw>
2025-01-18 23:51:20 +0800

    Put my keymap in major mode instead of global

--- lisp/init-perl.el
+++ lisp/init-perl.el
@@ -1,15 +1,18 @@
-(defvar rangi-cperl-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map "d" 'cperl-perldoc-at-point)
-    (define-key map "D" 'cperl-perldoc)
-    map)
-  "My keymap in cperl mode")
+(defun rangi-cperl-mode-map ()
+  (interactive)
+  (set-transient-map
+   (let ((rangi-cperl-mode-map (make-sparse-keymap)))
+     (define-key rangi-cperl-mode-map "d" 'cperl-perldoc-at-point)
+     (define-key rangi-cperl-mode-map "D" 'cperl-perldoc)
+     (which-key-show-keymap 'rangi-cperl-mode-map t)
+     rangi-cperl-mode-map)))
 
 (use-package cperl-mode
   :mode "\\.pl\\'"
   :interpreter "perl"
-  :bind-keymap (("s-m" . rangi-cperl-mode-map)
-                ("C-c m" . rangi-cperl-mode-map))
+  :bind (:map cperl-mode-map
+              ("s-m" . rangi-cperl-mode-map)
+              ("C-c m" . rangi-cperl-mode-map))
   :config
   (setq cperl-invalid-face nil)
   :hook ((cperl-mode . eglot-ensure)))
