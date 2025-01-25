3f1703413d071b5284d9b8c9a1b246de3f0744d4
Rangi Lin <me@rangilin.idv.tw>
2025-01-25 21:48:17 +0800

    add -B for showing bad file name

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -10,7 +10,7 @@
 (global-set-key (kbd "<S-f12>") (lambda () (interactive) (dired-other-window "~/")))
 
 ;; sort dired list
-(setq dired-listing-switches "-alF")
+(setq dired-listing-switches "-alBF")
 
 ;; don't ask about killing buffer visiting file
 (setq dired-clean-confirm-killing-deleted-buffers t)
