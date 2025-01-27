4cd18c8993b2f370cb54e7f44530b6e3d24c8e0a
Rangi Lin <me@rangilin.idv.tw>
2025-01-27 16:52:08 +0800

    make Dired follow symbolic link

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -10,7 +10,7 @@
 (global-set-key (kbd "<S-f12>") (lambda () (interactive) (dired-other-window "~/")))
 
 ;; sort dired list
-(setq dired-listing-switches "-alhBF")
+(setq dired-listing-switches "-alhBp")
 
 ;; don't ask about killing buffer visiting file
 (setq dired-clean-confirm-killing-deleted-buffers t)
