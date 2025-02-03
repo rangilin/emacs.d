738cc97482def3e6d39e957e74db2397e9c75b1b
Rangi Lin <me@rangilin.idv.tw>
2025-02-03 13:32:20 +0800

    fix rust server program not loading correctly

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -4,10 +4,11 @@
          (rust-mode . eglot-ensure))
   :config
   (setq rust-format-on-save t)
-  (setq rust-mode-treesitter-derive t))
+  (setq rust-mode-treesitter-derive t)
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs
+                 '((rust-ts-mode rust-mode) .
+                   ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))))
 
-(add-to-list 'eglot-server-programs
-             '((rust-ts-mode rust-mode) .
-               ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))
 
 (provide 'init-rust)
