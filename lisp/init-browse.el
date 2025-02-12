139c019f634448da131a3fbdc6d52d3e689ee7b5
Rangi Lin <me@rangilin.idv.tw>
2025-02-12 15:15:46 +0800

    clean up browse functions

--- lisp/init-browse.el
+++ lisp/init-browse.el
@@ -1,40 +1,25 @@
+;;; init-browse.el --- Browse stuff from Emacs  -*- lexical-binding: t -*-
+
 (require 'thingatpt)
 
-(defun rangi-search ()
-  "Search on the search engine"
+;; some existing functions
+(bind-key "C-c b o" 'browse-url-at-point)
+(bind-key "C-c b <mouse-1>" 'browse-url-at-mouse)
+
+
+;; browse search engine
+;; ---------------------------------------------------------------------
+(defun rangi-browse-search-engine ()
   (interactive)
-  (rangi-browse-url-with-prompt
+  (rangi--browse-url-with-prompt
    "Search in DuckDuckGo: "
    "https://duckduckgo.com/?q=%s"))
 
-(defun rangi-browse-url-with-prompt (prompt url-format)
-  "Browse URL with prompt as format arguments"
-  (interactive)
-  (let* ((result (rangi-prompt prompt))
-         (url (format url-format result)))
-    (unless (zerop (length result))
-      (browse-url url)
-      (message "Send %s to browser" url))))
-
-(defun rangi-prompt (prompt)
-  "Prompt with message `prompt' and with default value as initial content"
-  (interactive)
-  (read-from-minibuffer prompt (rangi-default-value)))
-
-(defun rangi-default-value ()
-  "Get default value, first it looks up text in region, then text at point. If not return empty string instead"
-  (cond
-   ((region-active-p) (buffer-substring-no-properties (mark) (point)))
-   ((let ((word (rangi-text-at-point)))
-      (when (> (length word) 0) word)))
-   (t "")))
-
-(defun rangi-text-at-point ()
-  "Get string at point without properties, nil if no word is found"
-  (let ((word (word-at-point)))
-    (when word (substring-no-properties word))))
+(bind-key "C-c b s" 'rangi-browse-search-engine)
 
 
+;; browse file with external app
+;; ---------------------------------------------------------------------
 ;; modify based on http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html
 (defun rangi-browse-in-external-app (&optional filename)
   "Open the current file or dired marked files in external app"
@@ -54,32 +39,50 @@
          (shell-command
           (concat "open " (shell-quote-argument fpath))))  file-list))))
 
+(bind-key "C-c b e" 'rangi-browse-in-external-app)
+(with-eval-after-load "dired"
+  (define-key dired-mode-map "E" 'rangi-browse-in-external-app))
 
-(defun rangi-shell-command-with-prompt (prompt url-format)
+
+;; browse dictionary
+;; ---------------------------------------------------------------------
+(defun rangi-browse-dictionary ()
+  (interactive)
+  (let ((result (rangi--prompt "Search in macOS dictionary: ")))
+    (unless (zerop (length result))
+      (shell-command (format "open dict://%s" result))
+      (message "Send %s to dictionary" result))))
+
+(bind-key "C-c b d" 'rangi-browse-dictionary)
+
+
+
+(defun rangi--browse-url-with-prompt (prompt url-format)
   "Browse URL with prompt as format arguments"
   (interactive)
-  (let* ((result (rangi-prompt prompt))
+  (let* ((result (rangi--prompt prompt))
          (url (format url-format result)))
     (unless (zerop (length result))
       (browse-url url)
       (message "Send %s to browser" url))))
 
-
-(defun rangi-browse-dictionary ()
+(defun rangi--prompt (prompt)
+  "Prompt with message `prompt' and with default value as initial content"
   (interactive)
-  (let ((result (rangi-prompt "Search in macOS dictionary: ")))
-    (unless (zerop (length result))
-      (shell-command (format "open dict://%s" result))
-      (message "Send %s to dictionary" result))))
+  (read-from-minibuffer prompt (rangi--default-value)))
 
+(defun rangi--default-value ()
+  "Get default value, first it looks up text in region, then text at point. If not return empty string instead"
+  (cond
+   ((region-active-p) (buffer-substring-no-properties (mark) (point)))
+   ((let ((word (rangi--text-at-point)))
+      (when (> (length word) 0) word)))
+   (t "")))
 
-(global-set-key (kbd "C-c b s") 'rangi-search)
-(global-set-key (kbd "C-c b p") 'browse-url-at-point)
-(global-set-key (kbd "C-c b <mouse-1>") 'browse-url-at-mouse)
-(global-set-key (kbd "C-c b o") 'rangi-browse-in-external-app)
-(with-eval-after-load "dired"
-  (define-key dired-mode-map "E" 'rangi-browse-in-external-app))
-(global-set-key (kbd "C-c b d") 'rangi-browse-dictionary)
+(defun rangi--text-at-point ()
+  "Get string at point without properties, nil if no word is found"
+  (let ((word (word-at-point)))
+    (when word (substring-no-properties word))))
 
 
 (provide 'init-browse)
