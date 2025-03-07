5613f5e8a27c1fc71ab7dece02a7417b4f03f48e
Rangi Lin <me@rangilin.idv.tw>
2025-03-07 19:28:44 +0800

    add recentf mode

--- lisp/init-file.el
+++ lisp/init-file.el
@@ -28,4 +28,26 @@
 
 
 
+;;;;;;;;;;;;;;;;;;
+;; Recent Files ;;
+;;;;;;;;;;;;;;;;;;
+
+(use-package recentf
+  :bind (("C-c f r" . recentf-open)
+	 ("C-c f R" . recentf-open-files))
+  :config
+  ;; put recentf file in designated cache directory
+  (setq recentf-save-file (expand-file-name "recentf" rangi-emacs-cache-directory))
+
+  ;; store more items
+  (setq recentf-max-menu-items 30)
+
+  ;; exlucde these entries
+  (setq recentf-exclude `("/ssh:"))
+
+  ;; add opened file into recentf when buffer list change
+  (add-hook 'buffer-list-update-hook #'recentf-track-opened-file)
+  (recentf-mode 1))
+
+
 (provide 'init-file)
