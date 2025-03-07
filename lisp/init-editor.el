5d68330c898a2b096479de94b20a33b338a00f2a
Rangi Lin <me@rangilin.idv.tw>
2025-03-07 16:27:14 +0800

    add ibuffer mode

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -34,6 +34,34 @@
 
 
 
+;; set up ibuffer
+(use-package ibuffer
+  :bind (("C-x C-b" . ibuffer))
+  :config
+  ;; don't show empty group
+  (setq-default ibuffer-show-empty-filter-groups nil)
+
+  ;; define a ibuffer column that show human readable size of the buffer
+  (define-ibuffer-column readable-size
+    (:name "Size" :inline t)
+    (cond
+     ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+     ((> (buffer-size) 1000) (format "%7.1fK" (/ (buffer-size) 1000.0)))
+     (t (format "%8d" (buffer-size)))))
+
+  ;; define ibuffer columns
+  (setq-default ibuffer-formats
+		'((mark modified read-only
+			" " (name 60 60 :left :elide)
+			" " (readable-size 9 -1 :right)
+			" " (mode 16 16 :left :elide)
+			" " filename-and-process)))
+
+  ;; unbind print so I don't accidentally print all of my buffers
+  (unbind-key "P" ibuffer-mode-map))
+
+
+
 ;;;;;;;;;;;;;;;
 ;; Auto Save ;;
 ;;;;;;;;;;;;;;;
