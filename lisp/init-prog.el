fd079c3d26ebaf72906b70fd6ab514f7ad32cfc6
Rangi Lin <me@rangilin.idv.tw>
2025-03-25 13:47:51 +0800

    set up programming and rust mode

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -7,6 +7,10 @@
 ;; show trailing whitespace in prog-mode
 (add-hook 'prog-mode-hook (lambda () (setq show-trailing-whitespace t)))
 
+;; show document with eldoc
+(use-package eldoc
+  :diminish eldoc-mode)
+
 
 
 ;;;;;;;;;;;;;;;;;;;
@@ -45,6 +49,7 @@
   (setq-default magit-diff-refine-hunk t))
 
 
+
 ;;;;;;;;;;;;;;
 ;; Web Mode ;;
 ;;;;;;;;;;;;;;
@@ -70,53 +75,80 @@
 
 
 
+;;;;;;;;;;;;;;;;;
+;; Error Check ;;
+;;;;;;;;;;;;;;;;;
 
-;; (use-package flymake
-;;   :bind (("M-n" . flymake-goto-next-error)
-;;          ("M-p" . flymake-goto-prev-error)
-;;          ("C-c l e" . rangi-flymake-show-buffer-diagnostics))
-;;   :config
-;;   (defun rangi-flymake-show-buffer-diagnostics ()
-;;     (interactive)
-;;     (let ((split-height-threshold 1))
-;;       (flymake-show-buffer-diagnostics)
-;;       (other-window 1))))
-
-
-
-;; (use-package company
-;;   :delight
-;;   :bind
-;;   ("M-." . company-complete)
-;;   (:map company-active-map
-;;         ("C-n" . company-select-next)
-;;         ("C-p" . company-select-previous))
-;;   :config
-;;   (setq company-tooltip-align-annotations t)
-;;   (setq company-format-margin-function 'company-text-icons-margin)
-;;   (setq company-text-face-extra-attributes '(:weight bold :slant italic))
-;;   (global-company-mode))
+(use-package flymake
+  :bind (("M-n" . flymake-goto-next-error)
+         ("M-p" . flymake-goto-prev-error)
+         ("C-c l e" . rangi-flymake-show-buffer-diagnostics))
+  :config
+  (defun rangi-flymake-show-buffer-diagnostics ()
+    (interactive)
+    (let ((split-height-threshold 1))
+      (flymake-show-buffer-diagnostics)
+      (other-window 1))))
 
 
-;; (use-package eldoc
-;;   :delight eldoc-mode)
 
-;; (use-package eglot
-;;   :bind (:map eglot-mode-map
-;; 	            ("C-c l h" . eldoc)
-;; 	            ("C-c l f" . eglot-format)
-;; 	            ("C-c l r" . eglot-rename))
-;;   :hook ((eglot-managed-mode . rangi-set-eglot-managed-mode))
-;;   :config
-;;   (setq eglot-autoshutdown t))
+(use-package eglot
+  :bind (:map eglot-mode-map
+	            ("C-c l h" . eldoc)
+	            ("C-c l f" . eglot-format)
+	            ("C-c l r" . eglot-rename))
+  :hook ((eglot-managed-mode . rangi-set-eglot-managed-mode))
+  :config
+
+  ;; Make eglot/eldoc works better with flymake diagnostics.
+  ;; https://github.com/joaotavora/eglot/discussions/898
+  (defun rangi-set-eglot-managed-mode ()
+    (setq eldoc-documentation-functions
+          (cons #'flymake-eldoc-function (remove #'flymake-eldoc-function eldoc-documentation-functions)))
+    (setq eldoc-documentation-strategy #'eldoc-documentation-compose))
+
+  (setq eglot-autoshutdown t))
 
 
-;; ;; Make eglot/eldoc works better with flymake diagnostics.
-;; ;; https://github.com/joaotavora/eglot/discussions/898
-;; (defun rangi-set-eglot-managed-mode ()
-;;   (setq eldoc-documentation-functions
-;;         (cons #'flymake-eldoc-function (remove #'flymake-eldoc-function eldoc-documentation-functions)))
-;;   (setq eldoc-documentation-strategy #'eldoc-documentation-compose))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Autocompleting ;;
+;;;;;;;;;;;;;;;;;;;;
+
+(use-package company
+  :diminish
+  :ensure t
+  :pin gnu
+  :bind
+  ("M-." . company-complete)
+  (:map company-active-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous))
+  :config
+  (setq company-tooltip-align-annotations t)
+  (setq company-format-margin-function 'company-text-icons-margin)
+  (setq company-text-face-extra-attributes '(:weight bold :slant italic))
+  (global-company-mode))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Miscellaneous programming modes ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Rust
+(use-package rust-mode
+  :ensure t
+  :pin nongnu
+  :mode (("\\.rs\\'" . rust-mode))
+  :hook ((rust-mode . eglot-ensure))
+  :config
+  (setq rust-format-on-save t)
+  (setq rust-mode-treesitter-derive t)
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs
+                 '((rust-ts-mode rust-mode) .
+                   ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))))
 
 
 
