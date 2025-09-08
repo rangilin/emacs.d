4c074d3a23a23b087c29793fdf660c4bab195d36
Rangi Lin <me@rangilin.idv.tw>
2025-09-08 17:30:14 +0800

    change column width for better visibility in half window

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -140,7 +140,7 @@
   ;; define ibuffer columns
   (setq-default ibuffer-formats
 		'((mark modified read-only
-			" " (name 60 60 :left :elide)
+			" " (name 32 32 :left :elide)
 			" " (readable-size 9 -1 :right)
 			" " (mode 16 16 :left :elide)
 			" " filename-and-process)))
