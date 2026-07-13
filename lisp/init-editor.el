88831bf9d0effaf4ee464ae37c4e08924b33a949
Rangi Lin <me@rangilin.idv.tw>
2026-07-13 11:33:41 +0800

    change font

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -48,7 +48,7 @@
 
 
 ;; fonts
-(let ((mono-spaced-font "JuliaMono")
+(let ((mono-spaced-font "Berkeley Mono")
       (proportionately-spaced-font "Noto Sans CJK TC"))
   (set-face-attribute 'default nil :family mono-spaced-font :height 140)
   (set-face-attribute 'fixed-pitch nil :family mono-spaced-font)
