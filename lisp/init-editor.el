19ad1d30ed4e84e5ddf4e378475a8e69867713a9
Rangi Lin <me@rangilin.idv.tw>
2026-07-18 13:23:44 +0800

    try out Aporetic font

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -48,7 +48,7 @@
 
 
 ;; fonts
-(let ((mono-spaced-font "Berkeley Mono")
+(let ((mono-spaced-font "Aporetic Serif Mono")
       (proportionately-spaced-font "Noto Sans CJK TC"))
   (set-face-attribute 'default nil :family mono-spaced-font :height 140)
   (set-face-attribute 'fixed-pitch nil :family mono-spaced-font)
