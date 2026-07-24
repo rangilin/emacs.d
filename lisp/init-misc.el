d22f6d6c94f6a602ae2f54ffc3e425207f0eba8c
Rangi Lin <me@rangilin.idv.tw>
2026-07-24 15:40:19 +0800

    configure models and prompts

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -103,22 +103,48 @@
     :load-path "site-lisp/inheritenv"))
 
 
-;; AI stuff
+;; LLM client
 (use-package gptel
   :pin nongnu
   :ensure t
-  :bind (("C-c a a" . gptel-send)
-         ("C-c a A" . gptel-menu))
+  :bind (("C-c a n" . rangi-gptel-new-session)
+         ("C-c a s" . gptel-send)
+         ("C-c a S" . gptel-menu))
+
+  :init
+  (setq rangi-gptel-directive-dev-plan "
+You are a senior software engineer acting as a collaborative partner working with me.
+I am solo dev that runs a one man software company, your goals is to help me clarifying my problems regarding software development and provide advices and answers best to your knowledge.
+
+Rules:
+- Be concise, direct and to the point.
+- Be thorough, but keep it grounded in practical, real-world execution.
+- Ask questions to clarify problems if missing important detail.
+")
+
+  ;; Set this before gptel is loaded so default directive can be pickup by it.
+  (setq-default gptel-directives
+                `((default . "You are a large language model living in Emacs and a helpful assistant. Respond concisely.")
+                  (dev-plan . ,(string-trim rangi-gptel-directive-dev-plan))))
+
+  (defun rangi-gptel-new-session ()
+    "Create a new, dedicated gtpel buffer with my own naming format"
+    (interactive)
+    (let* ((prefix (concat "llm-session-" (format-time-string "%Y%m%d%H%M") "-"))
+           (name (read-string "Creating new LLM session: " prefix)))
+      (switch-to-buffer (gptel name))))
+
   :config
-  (setq gptel-model 'google/gemini-3.1-flash-lite:online)
+  ;; set default model
+  (setq gptel-model 'deepseek/deepseek-v4-flash)
+  ;; configure backend, using OpenRouter
   (setq gptel-backend
         (gptel-make-openai "OpenRouter"
           :host "openrouter.ai"
           :endpoint "/api/v1/chat/completions"
           :stream t
           :key (lambda () (string-trim (shell-command-to-string "op read 'op://Personal/OpenRouter/API Keys/default'")))
-          :models '(google/gemini-3.1-flash-lite:online openai/gpt-5.4-nano openai/gpt-5.6-luna))))
-
+          :models '(deepseek/deepseek-v4-flash openai/gpt-5.4-nano openai/gpt-5.6-luna))))
 
 
 (provide 'init-misc)
