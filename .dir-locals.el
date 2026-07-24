5c91198f1db4e9d51f9dfd121ad979d1b26386a4
Rangi Lin <me@rangilin.idv.tw>
2026-07-24 16:18:36 +0800

    remove dir local file

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((jinx-dir-local-words . "inheritenv")))
- (".cache" . ((nil . ((buffer-read-only . t)))))
- ("site-lisp" . ((nil . ((buffer-read-only . t))))))
