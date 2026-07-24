17931eac8e1853896ac4fd84e6ff42db7476ed86
Rangi Lin <me@rangilin.idv.tw>
2026-07-24 15:40:36 +0800

    add spelling

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,6 @@
-((".cache" . ((nil . ((buffer-read-only . t)))))
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-dir-local-words . "inheritenv")))
+ (".cache" . ((nil . ((buffer-read-only . t)))))
  ("site-lisp" . ((nil . ((buffer-read-only . t))))))
